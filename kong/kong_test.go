357465b90001cf66cb94197cff8874f04483914d
Harry Bagdi <harrybagdi@gmail.com>
2018-04-03 21:01:37 -0700

    tests(/status) add a basic test for /status endpoint

---
+++ kong/kong_test.go
@@ -0,0 +1,19 @@
+package kong
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKongStatus(T *testing.T) {
+	assert := assert.New(T)
+
+	client, err := NewClient(nil, nil)
+	assert.Nil(err)
+	assert.NotNil(client)
+
+	status, err := client.Status()
+	assert.Nil(err)
+	assert.NotNil(status)
+}
