fdbee4b84a2a58c040f9b0df06c1b1da6159152c
Harry Bagdi <harrybagdi@gmail.com>
2018-08-31 14:59:56 -0700

    feat(/services) implement List() for services endpoint

--- kong/service_service.go
+++ kong/service_service.go
@@ -2,6 +2,7 @@ package kong
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 )
@@ -11,7 +12,7 @@ type Svcservice service
 
 // Create creates an Service in Kong
 // If an ID is specified, it will be used to
-// create a consumer in Kong, otherwise an ID
+// create a service in Kong, otherwise an ID
 // is auto-generated.
 func (s *Svcservice) Create(ctx context.Context, service *Service) (*Service, error) {
 
@@ -99,4 +100,28 @@ func (s *Svcservice) Delete(ctx context.Context, nameOrID *string) error {
 
 	_, err = s.client.Do(ctx, req, nil)
 	return err
+}
+
+// List fetches a list of Services in Kong.
+// opt can be used to control pagination.
+func (s *Svcservice) List(ctx context.Context, opt *ListOpt) ([]*Service, *ListOpt, error) {
+	data, next, err := s.client.list(ctx, "/services", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	var services []*Service
+	for _, object := range data {
+		b, err := object.MarshalJSON()
+		if err != nil {
+			return nil, nil, err
+		}
+		var service Service
+		err = json.Unmarshal(b, &service)
+		if err != nil {
+			return nil, nil, err
+		}
+		services = append(services, &service)
+	}
+
+	return services, next, nil
 }
--- kong/service_service_test.go
+++ kong/service_service_test.go
@@ -64,3 +64,83 @@ func TestServicesService(T *testing.T) {
 	_, err = client.Services.Update(defaultCtx, nil)
 	assert.NotNil(err)
 }
+
+func TestServiceListEndpoint(T *testing.T) {
+	assert := assert.New(T)
+
+	client, err := NewClient(nil, nil)
+	assert.Nil(err)
+	assert.NotNil(client)
+
+	// fixtures
+	services := []*Service{
+		&Service{
+			Name: String("foo1"),
+			Host: String("upstream1.com"),
+		},
+		&Service{
+			Name: String("foo2"),
+			Host: String("upstream2.com"),
+		},
+		&Service{
+			Name: String("foo3"),
+			Host: String("upstream3.com"),
+		},
+	}
+
+	// create fixturs
+	for i := 0; i < len(services); i++ {
+		service, err := client.Services.Create(defaultCtx, services[i])
+		assert.Nil(err)
+		assert.NotNil(service)
+		services[i] = service
+	}
+
+	servicesFromKong, next, err := client.Services.List(defaultCtx, nil)
+	assert.Nil(err)
+	assert.Nil(next)
+	assert.NotNil(servicesFromKong)
+	assert.Equal(3, len(servicesFromKong))
+
+	// check if we see all services
+	assert.True(compareServices(services, servicesFromKong))
+
+	// Test pagination
+	servicesFromKong = []*Service{}
+
+	// first page
+	page1, next, err := client.Services.List(defaultCtx, &ListOpt{Size: 1})
+	assert.Nil(err)
+	assert.NotNil(next)
+	assert.NotNil(page1)
+	assert.Equal(1, len(page1))
+	servicesFromKong = append(servicesFromKong, page1...)
+
+	// last page
+	next.Size = 2
+	page2, next, err := client.Services.List(defaultCtx, next)
+	assert.Nil(err)
+	assert.Nil(next)
+	assert.NotNil(page2)
+	assert.Equal(2, len(page2))
+	servicesFromKong = append(servicesFromKong, page2...)
+
+	assert.True(compareServices(services, servicesFromKong))
+
+	for i := 0; i < len(services); i++ {
+		assert.Nil(client.Services.Delete(defaultCtx, services[i].ID))
+	}
+}
+
+func compareServices(expected, actual []*Service) bool {
+	var expectedUsernames, actualUsernames []string
+	for _, service := range expected {
+		expectedUsernames = append(expectedUsernames, *service.Name)
+	}
+
+	for _, service := range actual {
+		actualUsernames = append(actualUsernames, *service.Name)
+	}
+
+	return (compareSlices(expectedUsernames, actualUsernames))
+}
