16319b86e7c1f2dd69d1449b9460bdef8560fd38
Harry Bagdi <harrybagdi@gmail.com>
2018-12-03 20:49:05 -0800

    chore(ci) bump up Kong to 1.0.0rc3

--- .ci/setup_kong.sh
+++ .ci/setup_kong.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+KONG_VERSION="1.0.0rc3"
 set e
 # download Kong deb
 
 sudo apt-get update
 sudo apt-get install openssl libpcre3 procps perl wget
 
-/usr/bin/curl -sL 'https://kong.bintray.com/kong-community-edition-deb/dists/kong-community-edition-0.14.0.trusty.all.deb' -o kong.deb -o kong.deb
+/usr/bin/curl -sL \
+  "https://kong.bintray.com/kong-community-edition-deb/dists/kong-community-edition-${KONG_VERSION}.trusty.all.deb" \
+  -o kong.deb -o kong.deb
 
 sudo dpkg -i kong.deb
 
-sudo kong migrations up
+sudo kong migrations bootstrap
 sudo kong start
