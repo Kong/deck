986b43c11a8b9e949f2f2a3dea15c35b67764f6d
Harry Bagdi <harrybagdi@gmail.com>
2018-12-26 17:51:57 -0800

    fix: introduce lock in colored print and fix a bug
    Println functions need only interfaces and not a format string.
    
    These functions are now goroutine safe, but the mutex can introduce
    contention. Use with care.

--- print/color.go
+++ print/color.go
@@ -1,21 +1,55 @@
 package print
 
 import (
+	"sync"
+
 	"github.com/fatih/color"
 )
 
 var (
-	// CreatePrintf is fmt.Printf with red as foreground color.
-	CreatePrintf = color.New(color.FgGreen).PrintfFunc()
-	// DeletePrintf is fmt.Printf with green as foreground color.
-	DeletePrintf = color.New(color.FgRed).PrintfFunc()
-	// UpdatePrintf is fmt.Printf with yellow as foreground color.
-	UpdatePrintf = color.New(color.FgYellow).PrintfFunc()
+	// mu is used to synchronize writes from multiple goroutines.
+	mu sync.Mutex
 
+	createPrintf = color.New(color.FgGreen).PrintfFunc()
+	// CreatePrintf is fmt.Printf with red as foreground color.
+	CreatePrintf = func(format string, a ...interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+		createPrintf(format, a...)
+	}
+	deletePrintf = color.New(color.FgRed).PrintfFunc()
+	// DeletePrintf is fmt.Printf with green as foreground color.
+	DeletePrintf = func(format string, a ...interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+		deletePrintf(format, a...)
+	}
+	updatePrintf = color.New(color.FgYellow).PrintfFunc()
+	// UpdatePrintf is fmt.Printf with yellow as foreground color.
+	UpdatePrintf = func(format string, a ...interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+		updatePrintf(format, a...)
+	}
+	createPrintln = color.New(color.FgGreen).PrintlnFunc()
 	// CreatePrintln is fmt.Println with red as foreground color.
-	CreatePrintln = color.New(color.FgGreen).PrintlnFunc()
+	CreatePrintln = func(a ...interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+		createPrintln(a...)
+	}
+	deletePrintln = color.New(color.FgRed).PrintlnFunc()
 	// DeletePrintln is fmt.Println with green as foreground color.
-	DeletePrintln = color.New(color.FgRed).PrintlnFunc()
+	DeletePrintln = func(a ...interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+		deletePrintln(a...)
+	}
+	updatePrintln = color.New(color.FgYellow).PrintlnFunc()
 	// UpdatePrintln is fmt.Println with yellow as foreground color.
-	UpdatePrintln = color.New(color.FgYellow).PrintlnFunc()
+	UpdatePrintln = func(a ...interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+		updatePrintln(a...)
+	}
 )
