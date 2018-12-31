300b773ccdae022c53fad177aaf8c491bcecaabd
Harry Bagdi <harrybagdi@gmail.com>
2018-12-31 15:37:14 -0800

    feat(file) sort entities deterministically before writing out
    The order in which records are returned by Kong is not defined.
    If a user performs a `deck dump` multiple times, the output can change.
    
    We now sort all entities before performing a write to the disk.

--- file/writer.go
+++ file/writer.go
@@ -2,6 +2,8 @@ package file
 
 import (
 	"io/ioutil"
+	"sort"
+	"strings"
 
 	"github.com/kong/deck/state"
 	yaml "gopkg.in/yaml.v2"
@@ -29,11 +31,18 @@ func KongStateToFile(kongState *state.KongState, filename string) error {
 			r.UpdatedAt = nil
 			s.Routes = append(s.Routes, &route{Route: r.Route})
 		}
+		sort.SliceStable(s.Routes, func(i, j int) bool {
+			return strings.Compare(*s.Routes[i].Name, *s.Routes[j].Name) < 0
+		})
 		s.ID = nil
 		s.CreatedAt = nil
 		s.UpdatedAt = nil
 		file.Services = append(file.Services, s)
 	}
+	sort.SliceStable(file.Services, func(i, j int) bool {
+		return strings.Compare(*file.Services[i].Name,
+			*file.Services[j].Name) < 0
+	})
 
 	upstreams, err := kongState.Upstreams.GetAll()
 	if err != nil {
@@ -51,10 +60,18 @@ func KongStateToFile(kongState *state.KongState, filename string) error {
 			t.CreatedAt = nil
 			u.Targets = append(u.Targets, &target{Target: t.Target})
 		}
+		sort.SliceStable(u.Targets, func(i, j int) bool {
+			return strings.Compare(*u.Targets[i].Target.Target,
+				*u.Targets[j].Target.Target) < 0
+		})
 		u.ID = nil
 		u.CreatedAt = nil
 		file.Upstreams = append(file.Upstreams, u)
 	}
+	sort.SliceStable(file.Upstreams, func(i, j int) bool {
+		return strings.Compare(*file.Upstreams[i].Name,
+			*file.Upstreams[j].Name) < 0
+	})
 
 	certificates, err := kongState.Certificates.GetAll()
 	if err != nil {
@@ -62,10 +79,17 @@ func KongStateToFile(kongState *state.KongState, filename string) error {
 	}
 	for _, c := range certificates {
 		c := certificate{Certificate: c.Certificate}
+		sort.SliceStable(c.SNIs, func(i, j int) bool {
+			return strings.Compare(*c.SNIs[i], *c.SNIs[j]) < 0
+		})
 		c.ID = nil
 		c.CreatedAt = nil
 		file.Certificates = append(file.Certificates, c)
 	}
+	sort.SliceStable(file.Certificates, func(i, j int) bool {
+		return strings.Compare(*file.Certificates[i].Cert,
+			*file.Certificates[j].Cert) < 0
+	})
 
 	c, err := yaml.Marshal(file)
 	err = ioutil.WriteFile(filename, c, 0600)
