e009a8de98413a188573ccae8baef1f2f47b73f2
Harry Bagdi <harrybagdi@gmail.com>
2019-04-05 18:36:32 -0700

    tests: add helper to run tests for specific versions

--- kong/kong_test.go
+++ kong/kong_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,4 +37,37 @@ func TestMain(m *testing.M) {
 	// to test ListAll code for pagination
 	pageSize = 1
 	os.Exit(m.Run())
+}
+
+var currentVersion semver.Version
+
+// runWhenKong skips the current test if the version of Kong doesn't
+// fall in the semverRange.
+// This helper function can be used in tests to write version specific
+// tests for Kong.
+func runWhenKong(t *testing.T, semverRange string) {
+	if currentVersion.Major == 0 {
+		client, err := NewClient(nil, nil)
+		if err != nil {
+			t.Error(err)
+		}
+		res, err := client.Root(nil)
+		if err != nil {
+			t.Error(err)
+		}
+		v := res["version"].(string)
+		currentVersion, err = semver.Parse(v)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	r, err := semver.ParseRange(semverRange)
+	if err != nil {
+		t.Error(err)
+	}
+	if !r(currentVersion) {
+		t.Skip()
+	}
+
 }
