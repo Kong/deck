445842347384411a0441a2612eaa24bb12f553ab
Harry Bagdi <harrybagdi@gmail.com>
2019-04-13 13:32:13 -0700

    chore(autogen) change autogen script to not rely on dep mgmt
    Previously, vendored code was used to run the autgen script.
    This commits is in preperation for move to Go modules.
    
    After this commit, we no longer rely on dependency listed in a lock file
    for auto-gen purposes.

--- hack/update-deepcopy-gen.sh
+++ hack/update-deepcopy-gen.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
+VERSION="50b56122"
 
-./vendor/k8s.io/code-generator/generate-groups.sh \
+if [[ ! -d /tmp/code-generator ]];
+then
+  git clone https://github.com/kubernetes/code-generator.git  /tmp/code-generator
+  pushd /tmp/code-generator
+  git checkout $VERSION
+  popd
+fi
+/tmp/code-generator/generate-groups.sh \
 deepcopy \
 github.com/hbagdi/go-kong/kong \
 github.com/hbagdi \
