98bc2fe45c3a314962d0b8b027f416961f45caad
Harry Bagdi <harrybagdi@gmail.com>
2019-08-10 15:49:09 -0700

    feat: add credential types

---
+++ kong/credentials.go
@@ -0,0 +1,51 @@
+package kong
+
+// KeyAuth represents a key-auth credential in Kong.
+// +k8s:deepcopy-gen=true
+type KeyAuth struct {
+	Consumer  *Consumer `json:"consumer,omitempty" yaml:"consumer,omitempty"`
+	CreatedAt *int      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	ID        *string   `json:"id,omitempty" yaml:"id,omitempty"`
+	Key       *string   `json:"key,omitempty" yaml:"key,omitempty"`
+}
+
+// BasicAuth represents a basic-auth credential in Kong.
+// +k8s:deepcopy-gen=true
+type BasicAuth struct {
+	Consumer  *Consumer `json:"consumer,omitempty" yaml:"consumer,omitempty"`
+	CreatedAt *int      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	ID        *string   `json:"id,omitempty" yaml:"id,omitempty"`
+	Username  *string   `json:"username,omitempty" yaml:"username,omitempty"`
+	Password  *string   `json:"password,omitempty" yaml:"password,omitempty"`
+}
+
+// HMACAuth represents a hmac-auth credential in Kong.
+// +k8s:deepcopy-gen=true
+type HMACAuth struct {
+	Consumer  *Consumer `json:"consumer,omitempty" yaml:"consumer,omitempty"`
+	CreatedAt *int      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	ID        *string   `json:"id,omitempty" yaml:"id,omitempty"`
+	Username  *string   `json:"username,omitempty" yaml:"username,omitempty"`
+	Secret    *string   `json:"secret,omitempty" yaml:"secret,omitempty"`
+}
+
+// JWTAuth represents a JWT credential in Kong.
+// +k8s:deepcopy-gen=true
+type JWTAuth struct {
+	Consumer     *Consumer `json:"consumer,omitempty" yaml:"consumer,omitempty"`
+	CreatedAt    *int      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	ID           *string   `json:"id,omitempty" yaml:"id,omitempty"`
+	Algorithm    *string   `json:"algorithm,omitempty" yaml:"algorithm,omitempty"`
+	Key          *string   `json:"key,omitempty" yaml:"key,omitempty"`
+	RSAPublicKey *string   `json:"rsa_public_key,omitempty" yaml:"rsa_public_key,omitempty"`
+	Secret       *string   `json:"secret,omitempty" yaml:"secret,omitempty"`
+}
+
+// HMACAuth represents a hmac-auth credential in Kong.
+// +k8s:deepcopy-gen=true
+type ACLGroup struct {
+	Consumer  *Consumer `json:"consumer,omitempty" yaml:"consumer,omitempty"`
+	CreatedAt *int      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	ID        *string   `json:"id,omitempty" yaml:"id,omitempty"`
+	Group     *string   `json:"group,omitempty" yaml:"group,omitempty"`
+}
