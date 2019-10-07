3ba8e98194c98d5642f4007f68519dfdf4e0224c
Harry Bagdi <harrybagdi@gmail.com>
2019-10-06 22:43:26 -0700

    feat(state) make username of consumer optional
    Consumers are now indexed via ID, which opens up doors for consumers
    with only custom ID set.

--- state/consumer.go
+++ state/consumer.go
@@ -2,7 +2,7 @@ package state
 
 import (
 	memdb "github.com/hashicorp/go-memdb"
-	"github.com/pkg/errors"
+	"github.com/hbagdi/deck/utils"
 )
 
 const (
@@ -13,16 +13,15 @@ var consumerTableSchema = &memdb.TableSchema{
 	Name: consumerTableName,
 	Indexes: map[string]*memdb.IndexSchema{
 		id: {
-			Name:   id,
-			Unique: true,
-			// EnforceUnique: true,
+			Name:    id,
+			Unique:  true,
 			Indexer: &memdb.StringFieldIndex{Field: "ID"},
 		},
 		"Username": {
-			Name:   "Username",
-			Unique: true,
-			// EnforceUnique: true,
-			Indexer: &memdb.StringFieldIndex{Field: "Username"},
+			Name:         "Username",
+			Unique:       true,
+			Indexer:      &memdb.StringFieldIndex{Field: "Username"},
+			AllowMissing: true,
 		},
 		all: allIndex,
 	},
@@ -32,69 +31,117 @@ var consumerTableSchema = &memdb.TableSchema{
 type ConsumersCollection collection
 
 // Add adds a consumer to the collection
+// An error is thrown if consumer.ID is empty.
 func (k *ConsumersCollection) Add(consumer Consumer) error {
+	if utils.Empty(consumer.ID) {
+		return errIDRequired
+	}
+
 	txn := k.db.Txn(true)
 	defer txn.Abort()
-	err := txn.Insert(consumerTableName, &consumer)
+
+	var searchBy []string
+	searchBy = append(searchBy, *consumer.ID)
+	if !utils.Empty(consumer.Username) {
+		searchBy = append(searchBy, *consumer.Username)
+	}
+	_, err := getConsumer(txn, searchBy...)
+	if err == nil {
+		return ErrAlreadyExists
+	} else if err != ErrNotFound {
+		return err
+	}
+
+	err = txn.Insert(consumerTableName, &consumer)
 	if err != nil {
-		return errors.Wrap(err, "insert failed")
+		return err
 	}
 	txn.Commit()
 	return nil
 }
 
+func getConsumer(txn *memdb.Txn, IDs ...string) (*Consumer, error) {
+	for _, id := range IDs {
+		res, err := multiIndexLookupUsingTxn(txn, consumerTableName,
+			[]string{"Username", "id"}, id)
+		if err == ErrNotFound {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+		consumer, ok := res.(*Consumer)
+		if !ok {
+			panic(unexpectedType)
+		}
+		return &Consumer{Consumer: *consumer.DeepCopy()}, nil
+	}
+	return nil, ErrNotFound
+}
+
 // Get gets a consumer by name or ID.
 func (k *ConsumersCollection) Get(userNameOrID string) (*Consumer, error) {
-	res, err := multiIndexLookup(k.db, consumerTableName,
-		[]string{"Username", id}, userNameOrID)
-	if err == ErrNotFound {
-		return nil, ErrNotFound
+	if userNameOrID == "" {
+		return nil, errIDRequired
 	}
 
-	if err != nil {
-		return nil, errors.Wrap(err, "consumer lookup failed")
-	}
-	if res == nil {
-		return nil, ErrNotFound
-	}
-	consumer, ok := res.(*Consumer)
-	if !ok {
-		panic("unexpected type found")
-	}
-
-	return &Consumer{Consumer: *consumer.DeepCopy()}, nil
+	txn := k.db.Txn(false)
+	defer txn.Abort()
+	return getConsumer(txn, userNameOrID)
 }
 
 // Update udpates an existing consumer.
 // It returns an error if the consumer is not already present.
 func (k *ConsumersCollection) Update(consumer Consumer) error {
-	// TODO check if entity is already present or not, throw error if present
 	// TODO abstract this in the go-memdb library itself
+	if utils.Empty(consumer.ID) {
+		return errIDRequired
+	}
+
 	txn := k.db.Txn(true)
 	defer txn.Abort()
-	err := txn.Insert(consumerTableName, &consumer)
+
+	err := deleteConsumer(txn, *consumer.ID)
 	if err != nil {
-		return errors.Wrap(err, "update failed")
+		return err
 	}
+
+	err = txn.Insert(consumerTableName, &consumer)
+	if err != nil {
+		return err
+	}
+
 	txn.Commit()
 	return nil
 }
 
-// Delete deletes a consumer by name or ID.
-func (k *ConsumersCollection) Delete(usernameOrID string) error {
-	consumer, err := k.Get(usernameOrID)
-
+func deleteConsumer(txn *memdb.Txn, userNameOrID string) error {
+	consumer, err := getConsumer(txn, userNameOrID)
 	if err != nil {
-		return errors.Wrap(err, "looking up consumer")
+		return err
+	}
+
+	err = txn.Delete(consumerTableName, consumer)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete deletes a consumer by name or ID.
+func (k *ConsumersCollection) Delete(userNameOrID string) error {
+	if userNameOrID == "" {
+		return errIDRequired
 	}
 
 	txn := k.db.Txn(true)
 	defer txn.Abort()
 
-	err = txn.Delete(consumerTableName, consumer)
+	err := deleteConsumer(txn, userNameOrID)
 	if err != nil {
-		return errors.Wrap(err, "delete failed")
+		return err
 	}
+
 	txn.Commit()
 	return nil
 }
@@ -106,14 +153,14 @@ func (k *ConsumersCollection) GetAll() ([]*Consumer, error) {
 
 	iter, err := txn.Get(consumerTableName, all, true)
 	if err != nil {
-		return nil, errors.Wrapf(err, "consumer lookup failed")
+		return nil, err
 	}
 
 	var res []*Consumer
 	for el := iter.Next(); el != nil; el = iter.Next() {
 		s, ok := el.(*Consumer)
 		if !ok {
-			panic("unexpected type found")
+			panic(unexpectedType)
 		}
 		res = append(res, &Consumer{Consumer: *s.DeepCopy()})
 	}
