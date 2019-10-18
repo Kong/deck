03bcb92cdf41b649e26f2e39b2579573e04fd8b5
Harry Bagdi <harrybagdi@gmail.com>
2019-10-18 16:45:54 -0700

    feat(state) implement method based indexer
    If some common objects implement an interface, indexing can be done
    using a GetX() method rather than using struct fields.
    
    This can be really powerful and help us implement a full-blown
    abstracted out in-memory abstracted out DB.

---
+++ state/indexers/methodIndexer.go
@@ -0,0 +1,50 @@
+package indexers
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// MethodIndexer is used to create an index based on a string returned
+// as a result of calling a method on the object.
+// It is assumed that the method has no arguments.
+type MethodIndexer struct {
+	// Method name to call to get the string to bulid the index on.
+	Method string
+}
+
+// FromObject take Obj and returns index key formed using
+// the fields.
+func (s *MethodIndexer) FromObject(obj interface{}) (bool, []byte, error) {
+	v := reflect.ValueOf(obj)
+
+	key := ""
+
+	method := v.MethodByName(s.Method)
+	resp := method.Call(nil)
+	if len(resp) != 1 {
+		return false, nil, fmt.Errorf("function call returned unexpected result")
+	}
+	key = fmt.Sprintf("%s", resp[0])
+
+	if key == "" {
+		return false, nil, nil
+	}
+	return true, []byte(key), nil
+}
+
+// FromArgs takes in a string and returns its byte form.
+func (s *MethodIndexer) FromArgs(args ...interface{}) ([]byte, error) {
+	blob := ""
+	for _, arg := range args {
+		s, ok := arg.(string)
+		if !ok {
+			return nil, fmt.Errorf("argument must be a string: %#v", arg)
+		}
+		blob += s
+	}
+	if blob == "" {
+		return nil, fmt.Errorf("empty args is not a valid value")
+	}
+	return []byte(blob), nil
+}
