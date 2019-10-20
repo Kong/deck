d3d359ec9946ef4d64fd0ddb060c7b27fb7ea7b5
Harry Bagdi <harrybagdi@gmail.com>
2019-10-19 21:57:09 -0700

    commit: chore(ci) run tests against 1.4.0rc2

--- .ci/setup_kong.sh
+++ .ci/setup_kong.sh
@@ -6,9 +6,19 @@ set e
 sudo apt-get update
 sudo apt-get install openssl libpcre3 procps perl wget
 
-/usr/bin/curl -sL \
-  "https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.trusty.all.deb" \
-  -o kong.deb
+# -_-
+# Kong broke the URL because of addition of arm support.
+
+SWITCH="1.3.100"
+
+URL="https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.trusty.all.deb"
+
+if [[ "$KONG_VERSION" > "$SWITCH" ]];
+then
+URL="https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.trusty.amd64.deb"
+fi
+
+/usr/bin/curl -sL $URL -o kong.deb
 
 sudo dpkg -i kong.deb
 
