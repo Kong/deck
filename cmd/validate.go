ad47106df43a69db8580af6fa04002ebd025e461
Harry Bagdi <harrybagdi@gmail.com>
2019-10-30 21:44:02 -0700

    feat(cmd) implement new validate command to validate input files

---
+++ cmd/validate.go
@@ -0,0 +1,65 @@
+// Copyright © 2019 Harry Bagdi <harrybagdi@gmail.com>
+
+package cmd
+
+import (
+	"github.com/hbagdi/deck/file"
+	"github.com/hbagdi/deck/state"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var (
+	validateCmdKongStateFile string
+)
+
+// validateCmd represents the diff command
+var validateCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "Diff the current entities in Kong with the on on disks",
+	Long: `Diff is like a dry run of 'decK sync' command.
+
+It will load entities form Kong and then perform a diff on those with
+the entities present in files locally. This allows you to see the entities
+that will be created or updated or deleted.
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// read target file
+		// this does json schema validation as well
+		targetContent, err := file.GetContentFromFile(validateCmdKongStateFile)
+		if err != nil {
+			return err
+		}
+
+		dummyEmptyState, err := state.NewKongState()
+		if err != nil {
+			return err
+		}
+
+		rawState, err := file.Get(targetContent, dummyEmptyState)
+		if err != nil {
+			return err
+		}
+		// this catches foreign relation errors
+		_, err = state.Get(rawState)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if validateCmdKongStateFile == "" {
+			return errors.New("A state file with Kong's configuration " +
+				"must be specified using -s/--state flag.")
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(validateCmd)
+	validateCmd.Flags().StringVarP(&validateCmdKongStateFile,
+		"state", "s", "kong.yaml", "file containing Kong's configuration. "+
+			"Use '-' to read from stdin.")
+}
