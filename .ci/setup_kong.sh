af73d5fa9d7aaa701b96bf492b53f6ea8658f857
Harry Bagdi <harrybagdi@gmail.com>
2020-05-15 15:05:17 -0700

    chore: run tests against kong enteprise

--- .ci/setup_kong.sh
+++ .ci/setup_kong.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-set e
+set -e
 # download Kong deb
 
 sudo apt-get update
 sudo apt-get install openssl libpcre3 procps perl wget
 
-# -_-
-# Kong broke the URL because of addition of arm support.
+function setup_kong(){
+  SWITCH="1.3.100"
 
-SWITCH="1.3.100"
+  URL="https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.xenial.all.deb"
 
-URL="https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.xenial.all.deb"
+  if [[ "$KONG_VERSION" > "$SWITCH" ]];
+  then
+  URL="https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.xenial.amd64.deb"
+  fi
 
-if [[ "$KONG_VERSION" > "$SWITCH" ]];
-then
-URL="https://kong.bintray.com/kong-deb/kong-${KONG_VERSION}.xenial.amd64.deb"
+  /usr/bin/curl -sL $URL -o kong.deb
+}
+
+function setup_kong_enterprise(){
+  KONG_VERSION=${KONG_VERSION#"enterprise-"}
+  URL="https://kong.bintray.com/kong-enterprise-edition-deb/dists/kong-enterprise-edition-${KONG_VERSION}.xenial.all.deb"
+  /usr/bin/curl -sL \
+    -u $KONG_ENTERPRISE_REPO_USERNAME:$KONG_ENTERPRISE_REPO_PASSSWORD \
+    $URL -o kong.deb
+}
+
+if [[ $KONG_VERSION == *"enterprise"* ]]; then
+  setup_kong_enterprise
+else
+  setup_kong
 fi
 
-/usr/bin/curl -sL $URL -o kong.deb
-
 sudo dpkg -i kong.deb
+echo $KONG_LICENSE_DATA | sudo tee /etc/kong/license.json
+export KONG_LICENSE_PATH=/tmp/license.json
 
 sudo kong migrations bootstrap
+sudo kong version
 sudo kong start
