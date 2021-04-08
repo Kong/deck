Kong/deck

commit 4717d531e15837e97bff2426b7c28b5861f4778c

chore: temporarily remove analytics from being sent (#301)

cmd/root.go
@@ -1,13 +1,11 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"io/ioutil"
 	"net/url"
 	"os"
 	"strings"
-	"sync"
 
 	"github.com/fatih/color"
 	"github.com/kong/deck/utils"
@@ -43,29 +41,8 @@ It can be used to export, import or sync entities to Kong.`,
 // Execute adds all child commands to the root command and sets
 // sflags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	var wg sync.WaitGroup
-	var err error
-	const threads = 2
-	wg.Add(threads)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	go func() {
-		defer wg.Done()
-		sendAnalytics(ctx)
-	}()
-
-	go func() {
-		defer wg.Done()
-		defer cancel()
-		err = rootCmd.Execute()
-	}()
-
-	wg.Wait()
-	if err != nil {
-		os.Exit(1)
-	}
+func Execute() error {
+	return rootCmd.Execute()
 }
 
 //nolint:errcheck

main.go
@@ -26,7 +26,11 @@ func registerSignalHandler() {
 
 func main() {
 	registerSignalHandler()
-	cmd.Execute()
+	err := cmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {

cmd/analytics.go
@@ -1,40 +1,0 @@
-package cmd
-
-import (
-	"context"
-	"net/http"
-	"os"
-)
-
-func sendAnalytics(ctx context.Context) {
-	const (
-		minOSArgs = 2
-	)
-
-	if os.Getenv("DECK_ANALYTICS") == "off" {
-		return
-	}
-
-	if len(os.Args) < minOSArgs {
-		return
-	}
-
-	cmd := os.Args[1]
-	if cmd == "help" ||
-		cmd == "ping" ||
-		cmd == "version" {
-		return
-	}
-
-	// HTTP to avoid latency due to handshake
-	URL := "http://d.yolo42.com/" + cmd
-
-	req, _ := http.NewRequestWithContext(ctx, "GET", URL, nil)
-	req.Header["deck-version"] = []string{VERSION}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return
-	}
-	resp.Body.Close()
-}

commit d1c9e0eb3b2b2ebd44026d4beeb9679ba51a2abe

hotfix: fix double error message prints
This incorrect change was introduced in 4717d53.

cmd/root.go
@@ -41,8 +41,12 @@ It can be used to export, import or sync entities to Kong.`,
 // Execute adds all child commands to the root command and sets
 // sflags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() error {
-	return rootCmd.Execute()
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		// do not print error because cobra already prints it
+		os.Exit(1)
+	}
 }
 
 //nolint:errcheck

main.go
@@ -26,11 +26,7 @@ func registerSignalHandler() {
 
 func main() {
 	registerSignalHandler()
-	err := cmd.Execute()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
 
 func init() {