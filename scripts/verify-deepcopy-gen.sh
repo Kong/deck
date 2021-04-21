f38e18d51d32196ecb3638048cb6fae38f5579e9
Harry <harrybagdi@gmail.com>
2021-04-21 13:02:17 -0500

    feat: implement DeepCopy methods for types in file package (#332)
    This patch also changes the order in which generated files are compared
    to ensure that the diff generated is semantically correct.

--- scripts/verify-deepcopy-gen.sh
+++ scripts/verify-deepcopy-gen.sh
@@ -4,10 +4,20 @@ go install k8s.io/code-generator/cmd/deepcopy-gen
 TMP_DIR=$(mktemp -d)
 trap "rm -rf $TMP_DIR" EXIT
 
+# konnect package
 deepcopy-gen --input-dirs github.com/kong/deck/konnect \
   -O zz_generated.deepcopy \
   --go-header-file scripts/header-template.go.tmpl \
   --output-base $TMP_DIR
 
-diff -Naur $TMP_DIR/github.com/kong/deck/konnect/zz_generated.deepcopy.go \
-  konnect/zz_generated.deepcopy.go
+diff -Naur konnect/zz_generated.deepcopy.go \
+  $TMP_DIR/github.com/kong/deck/konnect/zz_generated.deepcopy.go
+
+# file package
+deepcopy-gen --input-dirs github.com/kong/deck/file \
+  -O zz_generated.deepcopy \
+  --go-header-file scripts/header-template.go.tmpl \
+  --output-base $TMP_DIR
+
+diff -Naur file/zz_generated.deepcopy.go \
+  $TMP_DIR/github.com/kong/deck/file/zz_generated.deepcopy.go
