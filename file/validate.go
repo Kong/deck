96ffb5e25600ae1ada993e4328c92c2599975778
ccfishk <jimin.susan.hu@gmail.com>
2021-06-10 10:24:04 -0700

    improve product user experience

--- file/validate.go
+++ file/validate.go
@@ -1,12 +1,53 @@
 package file
 
 import (
+	"encoding/json"
 	"fmt"
 
 	ghodss "github.com/ghodss/yaml"
 	"github.com/kong/deck/utils"
 	"github.com/xeipuuv/gojsonschema"
 )
+
+func check(item string, t *string) bool {
+	if t != nil || len(*t) > 0 {
+		fmt.(item, "[err] is not allowed to be specified.")
+		return false
+	}
+	return true
+}
+
+func checkDefaults(kd KongDefaults) bool {
+	if ret := check("Service.ID", kd.Service.ID); !ret {
+		return ret
+	}
+
+	if ret := check("Service.Host", kd.Service.Host); !ret {
+		return false
+	}
+
+	if ret := check("Service.Name", kd.Service.Name); !ret {
+		return false
+	}
+
+	if ret := check("Route.Name", kd.Route.Name); !ret {
+		return false
+	}
+
+	if ret := check("Route.ID", kd.Route.ID); !ret {
+		return false
+	}
+
+	if ret := check("Upstream.Target", kd.Upstream.Name); !ret {
+		return false
+	}
+
+	if ret := check("Upstream.Target", kd.Upstream.ID); !ret {
+		return false
+	}
+
+	return true
+}
 
 func validate(content []byte) error {
 	var c map[string]interface{}
@@ -15,6 +56,17 @@ func validate(content []byte) error {
 		return fmt.Errorf("unmarshaling file content: %w", err)
 	}
 	c = ensureJSON(c)
+
+	var kongdefaults KongDefaults
+	err = json.Unmarshal(content, &kongdefaults)
+	if err != nil {
+		return fmt.Errorf("unmarshaling file into KongDefaults")
+	}
+	res := checkDefaults(kongdefaults)
+	if res == false {
+		return fmt.Errorf("fields are not allowed to specify in defaults")
+	}
+
 	schemaLoader := gojsonschema.NewStringLoader(kongJSONSchema)
 	documentLoader := gojsonschema.NewGoLoader(c)
 	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
