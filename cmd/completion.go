8a728b6a869a62eb948dcf9e95135ed04fd48ace
Gabriele Gerbino <gabrielegerbino@gmail.com>
2022-04-13 15:25:59 +0200

    docs: add code-blocks to completion's description

--- cmd/completion.go
+++ cmd/completion.go
@@ -14,7 +14,7 @@ func newCompletionCmd() *cobra.Command {
 		Long: `To load completions:
 
 Bash:
-
+` + "```" + `
   $ source <(deck completion bash)
 
   # To load completions for each session, execute once:
@@ -22,9 +22,9 @@ Bash:
   $ deck completion bash > /etc/bash_completion.d/deck
   # macOS:
   $ deck completion bash > /usr/local/etc/bash_completion.d/deck
-
+  ` + "```" + `
 Zsh:
-
+` + "```" + `
   # If shell completion is not already enabled in your environment,
   # you will need to enable it.  You can execute the following once:
 
@@ -34,22 +34,22 @@ Zsh:
   $ deck completion zsh > "${fpath[1]}/_yourprogram"
 
   # You will need to start a new shell for this setup to take effect.
-
+  ` + "```" + `
 fish:
-
+` + "```" + `
   $ deck completion fish | source
 
   # To load completions for each session, execute once:
   $ deck completion fish > ~/.config/fish/completions/deck.fish
-
+  ` + "```" + `
 PowerShell:
-
+` + "```" + `
   PS> deck completion powershell | Out-String | Invoke-Expression
 
   # To load completions for every new session, run:
   PS> deck completion powershell > deck.ps1
   # and source this file from your PowerShell profile.
-`,
+  ` + "```",
 		DisableFlagsInUseLine: true,
 		Args:                  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
