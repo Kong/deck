1bc841f0e3f25ed041f645b6e9b736675eb126d9
Gabriele Gerbino <gabrielegerbino@gmail.com>
2022-04-13 15:25:12 +0200

    docs: customize docs generation

---
+++ docs/utils.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/spf13/cobra"
+
+type byName []*cobra.Command
+
+func (s byName) Len() int           { return len(s) }
+func (s byName) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s byName) Less(i, j int) bool { return s[i].Name() < s[j].Name() }
+
+func hasSeeAlso(cmd *cobra.Command) bool {
+	if cmd.HasParent() {
+		return true
+	}
+	for _, c := range cmd.Commands() {
+		if !c.IsAvailableCommand() || c.IsAdditionalHelpTopicCommand() {
+			continue
+		}
+		return true
+	}
+	return false
+}
