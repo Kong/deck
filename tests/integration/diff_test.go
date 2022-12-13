Kong/deck

commit 6487bfd50eeba37eca58533aa117d5a2c7d889d1

refact(mask env vars): noMaskValues as a global var

diff/diff.go
@@ -55,6 +55,8 @@ type Syncer struct {
 	konnectClient *konnect.Client
 
 	entityDiffers map[types.EntityType]types.Differ
+
+	noMaskValues bool
 }
 
 type SyncerOpts struct {
@@ -66,6 +68,8 @@ type SyncerOpts struct {
 
 	SilenceWarnings bool
 	StageDelaySec   int
+
+	NoMaskValues bool
 }
 
 // NewSyncer constructs a Syncer.
@@ -79,6 +83,8 @@ func NewSyncer(opts SyncerOpts) (*Syncer, error) {
 
 		silenceWarnings: opts.SilenceWarnings,
 		stageDelaySec:   opts.StageDelaySec,
+
+		noMaskValues: opts.NoMaskValues,
 	}
 
 	err := s.init()
@@ -349,7 +355,7 @@ func generateDiffString(e crud.Event, isDelete bool, noMaskValues bool) (string,
 }
 
 // Solve generates a diff and walks the graph.
-func (sc *Syncer) Solve(ctx context.Context, parallelism int, dry bool, noMaskValues bool) (Stats, []error) {
+func (sc *Syncer) Solve(ctx context.Context, parallelism int, dry bool) (Stats, []error) {
 	stats := Stats{
 		CreateOps: &utils.AtomicInt32Counter{},
 		UpdateOps: &utils.AtomicInt32Counter{},
@@ -373,19 +379,19 @@ func (sc *Syncer) Solve(ctx context.Context, parallelism int, dry bool, noMaskVa
 		c := e.Obj.(state.ConsoleString)
 		switch e.Op {
 		case crud.Create:
-			diffString, err := generateDiffString(e, false, noMaskValues)
+			diffString, err := generateDiffString(e, false, sc.noMaskValues)
 			if err != nil {
 				return nil, err
 			}
 			cprint.CreatePrintln("creating", e.Kind, c.Console(), diffString)
 		case crud.Update:
-			diffString, err := generateDiffString(e, false, noMaskValues)
+			diffString, err := generateDiffString(e, false, sc.noMaskValues)
 			if err != nil {
 				return nil, err
 			}
 			cprint.UpdatePrintln("updating", e.Kind, c.Console(), diffString)
 		case crud.Delete:
-			diffString, err := generateDiffString(e, true, noMaskValues)
+			diffString, err := generateDiffString(e, true, sc.noMaskValues)
 			if err != nil {
 				return nil, err
 			}

cmd/diff.go
@@ -7,11 +7,10 @@ import (
 )
 
 var (
-	diffCmdKongStateFile          []string
-	diffCmdParallelism            int
-	diffCmdNoMaskDeckEnvVarsValue bool
-	diffCmdNonZeroExitCode        bool
-	diffWorkspace                 string
+	diffCmdKongStateFile   []string
+	diffCmdParallelism     int
+	diffCmdNonZeroExitCode bool
+	diffWorkspace          string
 )
 
 // newDiffCmd represents the diff command
@@ -28,7 +27,7 @@ that will be created, updated, or deleted.
 		Args: validateNoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return syncMain(cmd.Context(), diffCmdKongStateFile, true,
-				diffCmdParallelism, 0, diffWorkspace, diffCmdNoMaskDeckEnvVarsValue)
+				diffCmdParallelism, 0, diffWorkspace)
 		},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			if len(diffCmdKongStateFile) == 0 {
@@ -52,7 +51,7 @@ that will be created, updated, or deleted.
 			"any plugins associated with consumers")
 	diffCmd.Flags().IntVar(&diffCmdParallelism, "parallelism",
 		10, "Maximum number of concurrent operations.")
-	diffCmd.Flags().BoolVar(&diffCmdNoMaskDeckEnvVarsValue, "no-mask-deck-env-vars-value",
+	diffCmd.Flags().BoolVar(&noMaskValues, "no-mask-deck-env-vars-value",
 		false, "do not mask DECK_ environment variable values at diff output.")
 	diffCmd.Flags().StringSliceVar(&dumpConfig.SelectorTags,
 		"select-tag", []string{},

cmd/common.go
@@ -27,8 +27,9 @@ const (
 )
 
 var (
-	dumpConfig dump.Config
-	assumeYes  bool
+	dumpConfig   dump.Config
+	assumeYes    bool
+	noMaskValues bool
 )
 
 type mode int
@@ -81,7 +82,7 @@ func getWorkspaceName(workspaceFlag string, targetContent *file.Content) string 
 }
 
 func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
-	delay int, workspace string, noMaskValues bool,
+	delay int, workspace string,
 ) error {
 	// read target file
 	targetContent, err := file.GetContentFromFiles(filenames)
@@ -225,7 +226,7 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return err
 	}
 
-	totalOps, err := performDiff(ctx, currentState, targetState, dry, parallelism, delay, kongClient, noMaskValues)
+	totalOps, err := performDiff(ctx, currentState, targetState, dry, parallelism, delay, kongClient)
 	if err != nil {
 		return err
 	}
@@ -273,19 +274,20 @@ func fetchCurrentState(ctx context.Context, client *kong.Client, dumpConfig dump
 }
 
 func performDiff(ctx context.Context, currentState, targetState *state.KongState,
-	dry bool, parallelism int, delay int, client *kong.Client, noMaskValues bool,
+	dry bool, parallelism int, delay int, client *kong.Client,
 ) (int, error) {
 	s, err := diff.NewSyncer(diff.SyncerOpts{
 		CurrentState:  currentState,
 		TargetState:   targetState,
 		KongClient:    client,
 		StageDelaySec: delay,
+		NoMaskValues:  noMaskValues,
 	})
 	if err != nil {
 		return 0, err
 	}
 
-	stats, errs := s.Solve(ctx, parallelism, dry, noMaskValues)
+	stats, errs := s.Solve(ctx, parallelism, dry)
 	// print stats before error to report completed operations
 	printStats(stats)
 	if errs != nil {

cmd/common_konnect.go
@@ -123,7 +123,7 @@ func GetKongClientForKonnectMode(
 	})
 }
 
-func resetKonnectV2(ctx context.Context, resetNoMaskDeckEnvVarsValue bool) error {
+func resetKonnectV2(ctx context.Context) error {
 	client, err := GetKongClientForKonnectMode(ctx, konnectConfig)
 	if err != nil {
 		return err
@@ -136,7 +136,7 @@ func resetKonnectV2(ctx context.Context, resetNoMaskDeckEnvVarsValue bool) error
 	if err != nil {
 		return err
 	}
-	_, err = performDiff(ctx, currentState, targetState, false, 10, 0, client, resetNoMaskDeckEnvVarsValue)
+	_, err = performDiff(ctx, currentState, targetState, false, 10, 0, client)
 	if err != nil {
 		return err
 	}
@@ -170,7 +170,7 @@ func dumpKonnectV2(ctx context.Context) error {
 }
 
 func syncKonnect(ctx context.Context,
-	filenames []string, dry bool, parallelism int, noMaskValues bool,
+	filenames []string, dry bool, parallelism int,
 ) error {
 	httpClient := utils.HTTPClient()
 
@@ -244,12 +244,13 @@ func syncKonnect(ctx context.Context,
 		TargetState:   targetState,
 		KongClient:    kongClient,
 		KonnectClient: konnectClient,
+		NoMaskValues:  noMaskValues,
 	})
 	if err != nil {
 		return err
 	}
 
-	stats, errs := s.Solve(ctx, parallelism, dry, noMaskValues)
+	stats, errs := s.Solve(ctx, parallelism, dry)
 	// print stats before error to report completed operations
 	printStats(stats)
 	if errs != nil {

cmd/konnect_diff.go
@@ -7,10 +7,9 @@ import (
 )
 
 var (
-	konnectDiffCmdKongStateFile          []string
-	konnectDiffCmdParallelism            int
-	konnectDiffCmdNoMaskDeckEnvVarsValue bool
-	konnectDiffCmdNonZeroExitCode        bool
+	konnectDiffCmdKongStateFile   []string
+	konnectDiffCmdParallelism     int
+	konnectDiffCmdNonZeroExitCode bool
 )
 
 // newKonnectDiffCmd represents the 'deck konnect diff' command.
@@ -33,7 +32,7 @@ func newKonnectDiffCmd() *cobra.Command {
 				konnectConfig.Address = defaultLegacyKonnectURL
 			}
 			return syncKonnect(cmd.Context(), konnectDiffCmdKongStateFile, true,
-				konnectDiffCmdParallelism, konnectDiffCmdNoMaskDeckEnvVarsValue)
+				konnectDiffCmdParallelism)
 		},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			return preRunSilenceEventsFlag()
@@ -48,7 +47,7 @@ func newKonnectDiffCmd() *cobra.Command {
 			"with consumers.")
 	konnectDiffCmd.Flags().IntVar(&konnectDiffCmdParallelism, "parallelism",
 		100, "Maximum number of concurrent operations.")
-	konnectDiffCmd.Flags().BoolVar(&konnectDiffCmdNoMaskDeckEnvVarsValue, "no-mask-deck-env-vars-value",
+	konnectDiffCmd.Flags().BoolVar(&noMaskValues, "no-mask-deck-env-vars-value",
 		false, "do not mask DECK_ environment variable values at diff output.")
 	konnectDiffCmd.Flags().BoolVar(&konnectDiffCmdNonZeroExitCode, "non-zero-exit-code",
 		false, "return exit code 2 if there is a diff present,\n"+

cmd/konnect_sync.go
@@ -24,7 +24,7 @@ to get Konnect's state in sync with the input state.` + konnectAlphaState,
 				konnectConfig.Address = defaultLegacyKonnectURL
 			}
 			return syncKonnect(cmd.Context(), konnectDiffCmdKongStateFile, false,
-				konnectDiffCmdParallelism, konnectDiffCmdNoMaskDeckEnvVarsValue)
+				konnectDiffCmdParallelism)
 		},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			return preRunSilenceEventsFlag()
@@ -39,7 +39,7 @@ to get Konnect's state in sync with the input state.` + konnectAlphaState,
 			"with consumers.")
 	konnectSyncCmd.Flags().IntVar(&konnectDiffCmdParallelism, "parallelism",
 		100, "Maximum number of concurrent operations.")
-	konnectSyncCmd.Flags().BoolVar(&konnectDiffCmdNoMaskDeckEnvVarsValue, "no-mask-deck-env-vars-value",
+	konnectSyncCmd.Flags().BoolVar(&noMaskValues, "no-mask-deck-env-vars-value",
 		false, "do not mask DECK_ environment variable values at diff output.")
 	addSilenceEventsFlag(konnectSyncCmd.Flags())
 	return konnectSyncCmd

cmd/reset.go
@@ -9,10 +9,9 @@ import (
 )
 
 var (
-	resetCmdForce               bool
-	resetWorkspace              string
-	resetAllWorkspaces          bool
-	resetNoMaskDeckEnvVarsValue bool
+	resetCmdForce      bool
+	resetWorkspace     string
+	resetAllWorkspaces bool
 )
 
 // newResetCmd represents the reset command
@@ -48,7 +47,7 @@ By default, this command will ask for confirmation.`,
 			mode := getMode(nil)
 			if mode == modeKonnect {
 				_ = sendAnalytics("reset", "", mode)
-				return resetKonnectV2(ctx, resetNoMaskDeckEnvVarsValue)
+				return resetKonnectV2(ctx)
 			}
 
 			rootClient, err := utils.GetKongClient(rootConfig)
@@ -100,7 +99,7 @@ By default, this command will ask for confirmation.`,
 				if err != nil {
 					return err
 				}
-				_, err = performDiff(ctx, currentState, targetState, false, 10, 0, wsClient, resetNoMaskDeckEnvVarsValue)
+				_, err = performDiff(ctx, currentState, targetState, false, 10, 0, wsClient)
 				if err != nil {
 					return err
 				}
@@ -119,7 +118,7 @@ By default, this command will ask for confirmation.`,
 			"(Kong Enterprise only).")
 	resetCmd.Flags().BoolVar(&resetAllWorkspaces, "all-workspaces",
 		false, "reset configuration of all workspaces (Kong Enterprise only).")
-	resetCmd.Flags().BoolVar(&resetNoMaskDeckEnvVarsValue, "no-mask-deck-env-vars-value",
+	resetCmd.Flags().BoolVar(&noMaskValues, "no-mask-deck-env-vars-value",
 		false, "do not mask DECK_ environment variable values at diff output.")
 	resetCmd.Flags().StringSliceVar(&dumpConfig.SelectorTags,
 		"select-tag", []string{},

cmd/sync.go
@@ -7,10 +7,9 @@ import (
 )
 
 var (
-	syncCmdParallelism            int
-	syncCmdDBUpdateDelay          int
-	syncCmdNoMaskDeckEnvVarsValue bool
-	syncWorkspace                 string
+	syncCmdParallelism   int
+	syncCmdDBUpdateDelay int
+	syncWorkspace        string
 )
 
 // newSyncCmd represents the sync command
@@ -25,7 +24,7 @@ to get Kong's state in sync with the input state.`,
 		Args: validateNoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return syncMain(cmd.Context(), syncCmdKongStateFile, false,
-				syncCmdParallelism, syncCmdDBUpdateDelay, syncWorkspace, syncCmdNoMaskDeckEnvVarsValue)
+				syncCmdParallelism, syncCmdDBUpdateDelay, syncWorkspace)
 		},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			if len(syncCmdKongStateFile) == 0 {
@@ -49,7 +48,7 @@ to get Kong's state in sync with the input state.`,
 			"any plugins associated with consumers.")
 	syncCmd.Flags().IntVar(&syncCmdParallelism, "parallelism",
 		10, "Maximum number of concurrent operations.")
-	syncCmd.Flags().BoolVar(&syncCmdNoMaskDeckEnvVarsValue, "no-mask-deck-env-vars-value",
+	syncCmd.Flags().BoolVar(&noMaskValues, "no-mask-deck-env-vars-value",
 		false, "do not mask DECK_ environment variable values at diff output.")
 	syncCmd.Flags().StringSliceVar(&dumpConfig.SelectorTags,
 		"select-tag", []string{},

commit a196ec2d71ad7f89348c821c3768ca7ace93881e

fix: don't build diff doc for creates and deletes
This commit fixes a 'visual' regression introduced
in https://github.com/Kong/deck/pull/463, which is
making decK print full diff documents for 'create'
and 'delete' actions. This can result in very
verbose output in case of configuration files
containing hundreds/thousands of resources.

diff/diff.go
@@ -401,11 +401,7 @@ func (sc *Syncer) Solve(ctx context.Context, parallelism int, dry bool) (Stats, 
 		c := e.Obj.(state.ConsoleString)
 		switch e.Op {
 		case crud.Create:
-			diffString, err := generateDiffString(e, false, sc.noMaskValues)
-			if err != nil {
-				return nil, err
-			}
-			sc.createPrintln("creating", e.Kind, c.Console(), diffString)
+			sc.createPrintln("creating", e.Kind, c.Console())
 		case crud.Update:
 			diffString, err := generateDiffString(e, false, sc.noMaskValues)
 			if err != nil {
@@ -413,11 +409,7 @@ func (sc *Syncer) Solve(ctx context.Context, parallelism int, dry bool) (Stats, 
 			}
 			sc.updatePrintln("updating", e.Kind, c.Console(), diffString)
 		case crud.Delete:
-			diffString, err := generateDiffString(e, true, sc.noMaskValues)
-			if err != nil {
-				return nil, err
-			}
-			sc.deletePrintln("deleting", e.Kind, c.Console(), diffString)
+			sc.deletePrintln("deleting", e.Kind, c.Console())
 		default:
 			panic("unknown operation " + e.Op.String())
 		}

tests/integration/diff_test.go
@@ -11,14 +11,17 @@ import (
 )
 
 var (
-	expectedOutputMasked = `creating workspace test
-creating service svc1  {
-+  "connect_timeout": 60000
-+  "host": "[masked]"
-+  "id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d"
-+  "name": "svc1"
-+  "protocol": "http"
-+  "read_timeout": 60000
+	expectedOutputMasked = `updating service svc1  {
+   "connect_timeout": 60000,
+   "enabled": true,
+   "host": "[masked]",
+   "id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d",
+   "name": "svc1",
+   "port": 80,
+   "protocol": "http",
+   "read_timeout": 60000,
+   "retries": 5,
+   "write_timeout": 60000
 +  "tags": [
 +    "[masked] is an external host. I like [masked]!",
 +    "foo:foo",
@@ -26,52 +29,35 @@ creating service svc1  {
 +    "another:[masked]",
 +    "bar:[masked]"
 +  ]
-+  "write_timeout": 60000
  }
 
-creating plugin rate-limiting (global)  {
-+  "config": {
-+    "minute": 123
-+  }
-+  "id": "a1368a28-cb5c-4eee-86d8-03a6bdf94b5e"
-+  "name": "rate-limiting"
- }
-
+creating plugin rate-limiting (global)
 Summary:
-  Created: 2
-  Updated: 0
+  Created: 1
+  Updated: 1
   Deleted: 0
 `
 
-	expectedOutputUnMasked = `creating workspace test
-creating service svc1  {
-+  "connect_timeout": 60000
-+  "host": "mockbin.org"
-+  "id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d"
-+  "name": "svc1"
-+  "protocol": "http"
-+  "read_timeout": 60000
+	expectedOutputUnMasked = `updating service svc1  {
+   "connect_timeout": 60000,
+   "enabled": true,
+   "host": "mockbin.org",
+   "id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d",
+   "name": "svc1",
+   "port": 80,
+   "protocol": "http",
+   "read_timeout": 60000,
+   "retries": 5,
+   "write_timeout": 60000
 +  "tags": [
-+    "mockbin.org is an external host. I like mockbin.org!",
-+    "foo:foo",
-+    "baz:bazbaz",
-+    "another:bazbaz",
-+    "bar:barbar"
++    "test"
 +  ]
-+  "write_timeout": 60000
  }
 
-creating plugin rate-limiting (global)  {
-+  "config": {
-+    "minute": 123
-+  }
-+  "id": "a1368a28-cb5c-4eee-86d8-03a6bdf94b5e"
-+  "name": "rate-limiting"
- }
-
+creating plugin rate-limiting (global)
 Summary:
-  Created: 2
-  Updated: 0
+  Created: 1
+  Updated: 1
   Deleted: 0
 `
 
@@ -87,113 +73,108 @@ Summary:
 // test scope:
 //   - 1.x
 //   - 2.x
-func Test_Diff_Workspace_UnMasked_OlderThan3x(t *testing.T) {
+func Test_Diff_Workspace_OlderThan3x(t *testing.T) {
 	tests := []struct {
 		name          string
 		stateFile     string
 		expectedState utils.KongRawState
-		envVars       map[string]string
 	}{
 		{
 			name:      "diff with not existent workspace doesn't error out",
 			stateFile: "testdata/diff/001-not-existing-workspace/kong.yaml",
-			envVars:   diffEnvVars,
 		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			for k, v := range tc.envVars {
-				os.Setenv(k, v)
-				defer func(k string) {
-					os.Unsetenv(k)
-				}(k)
-			}
 			runWhen(t, "kong", "<3.0.0")
 			teardown := setup(t)
 			defer teardown(t)
 
-			out, err := diff(tc.stateFile, "--no-mask-deck-env-vars-value")
+			_, err := diff(tc.stateFile)
 			assert.NoError(t, err)
-			assert.Equal(t, out, expectedOutputUnMasked)
-		})
-	}
-}
-func Test_Diff_Workspace_Masked_OlderThan3x(t *testing.T) {
-	tests := []struct {
-		name          string
-		stateFile     string
-		expectedState utils.KongRawState
-		envVars       map[string]string
-	}{
-		{
-			name:      "diff with not existent workspace doesn't error out",
-			stateFile: "testdata/diff/001-not-existing-workspace/kong.yaml",
-			envVars:   diffEnvVars,
-		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			for k, v := range tc.envVars {
-				os.Setenv(k, v)
-				defer func(k string) {
-					os.Unsetenv(k)
-				}(k)
-			}
-			runWhen(t, "kong", "<3.0.0")
-			teardown := setup(t)
-			defer teardown(t)
-
-			out, err := diff(tc.stateFile)
-			assert.NoError(t, err)
-			assert.Equal(t, out, expectedOutputMasked)
 		})
 	}
 }
 
 // test scope:
 //   - 3.x
-func Test_Diff_Workspace_UnMasked_NewerThan3x(t *testing.T) {
+func Test_Diff_Workspace_NewerThan3x(t *testing.T) {
 	tests := []struct {
 		name          string
 		stateFile     string
 		expectedState utils.KongRawState
-		envVars       map[string]string
 	}{
 		{
 			name:      "diff with not existent workspace doesn't error out",
 			stateFile: "testdata/diff/001-not-existing-workspace/kong3x.yaml",
-			envVars:   diffEnvVars,
 		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			for k, v := range tc.envVars {
-				os.Setenv(k, v)
-				defer func(k string) {
-					os.Unsetenv(k)
-				}(k)
-			}
 			runWhen(t, "kong", ">=3.0.0")
 			teardown := setup(t)
 			defer teardown(t)
 
-			out, err := diff(tc.stateFile, "--no-mask-deck-env-vars-value")
+			_, err := diff(tc.stateFile)
 			assert.NoError(t, err)
-			assert.Equal(t, out, expectedOutputUnMasked)
 		})
 	}
 }
-func Test_Diff_Workspace_Masked_NewerThan3x(t *testing.T) {
+
+// test scope:
+//   - 2.8.0
+func Test_Diff_Masked_OlderThan3x(t *testing.T) {
 	tests := []struct {
-		name          string
-		stateFile     string
-		expectedState utils.KongRawState
-		envVars       map[string]string
+		name             string
+		initialStateFile string
+		stateFile        string
+		expectedState    utils.KongRawState
+		envVars          map[string]string
 	}{
 		{
-			name:      "diff with not existent workspace doesn't error out",
-			stateFile: "testdata/diff/001-not-existing-workspace/kong3x.yaml",
-			envVars:   diffEnvVars,
+			name:             "env variable are masked",
+			initialStateFile: "testdata/diff/002-mask/initial.yaml",
+			stateFile:        "testdata/diff/002-mask/kong.yaml",
+			envVars:          diffEnvVars,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			for k, v := range tc.envVars {
+				os.Setenv(k, v)
+				defer func(k string) {
+					os.Unsetenv(k)
+				}(k)
+			}
+			runWhen(t, "kong", "==2.8.0")
+			teardown := setup(t)
+			defer teardown(t)
+
+			// initialize state
+			assert.NoError(t, sync(tc.initialStateFile))
+
+			out, err := diff(tc.stateFile)
+			assert.NoError(t, err)
+			assert.Equal(t, expectedOutputMasked, out)
+		})
+	}
+}
+
+// test scope:
+//   - 3.x
+func Test_Diff_Masked_NewerThan3x(t *testing.T) {
+	tests := []struct {
+		name             string
+		initialStateFile string
+		stateFile        string
+		expectedState    utils.KongRawState
+		envVars          map[string]string
+	}{
+		{
+			name:             "env variable are masked",
+			initialStateFile: "testdata/diff/002-mask/initial3x.yaml",
+			stateFile:        "testdata/diff/002-mask/kong3x.yaml",
+			envVars:          diffEnvVars,
 		},
 	}
 	for _, tc := range tests {
@@ -208,9 +189,90 @@ func Test_Diff_Workspace_Masked_NewerThan3x(t *testing.T) {
 			teardown := setup(t)
 			defer teardown(t)
 
+			// initialize state
+			assert.NoError(t, sync(tc.initialStateFile))
+
 			out, err := diff(tc.stateFile)
 			assert.NoError(t, err)
-			assert.Equal(t, out, expectedOutputMasked)
+			assert.Equal(t, expectedOutputMasked, out)
+		})
+	}
+}
+
+// test scope:
+//   - 2.8.0
+func Test_Diff_Unasked_OlderThan3x(t *testing.T) {
+	tests := []struct {
+		name             string
+		initialStateFile string
+		stateFile        string
+		expectedState    utils.KongRawState
+		envVars          map[string]string
+	}{
+		{
+			name:             "env variable are unmasked",
+			initialStateFile: "testdata/diff/003-unmask/initial.yaml",
+			stateFile:        "testdata/diff/003-unmask/kong.yaml",
+			envVars:          diffEnvVars,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			for k, v := range tc.envVars {
+				os.Setenv(k, v)
+				defer func(k string) {
+					os.Unsetenv(k)
+				}(k)
+			}
+			runWhen(t, "kong", "==2.8.0")
+			teardown := setup(t)
+			defer teardown(t)
+
+			// initialize state
+			assert.NoError(t, sync(tc.initialStateFile))
+
+			out, err := diff(tc.stateFile, "--no-mask-deck-env-vars-value")
+			assert.NoError(t, err)
+			assert.Equal(t, expectedOutputUnMasked, out)
+		})
+	}
+}
+
+// test scope:
+//   - 3.x
+func Test_Diff_Unasked_NewerThan3x(t *testing.T) {
+	tests := []struct {
+		name             string
+		initialStateFile string
+		stateFile        string
+		expectedState    utils.KongRawState
+		envVars          map[string]string
+	}{
+		{
+			name:             "env variable are unmasked",
+			initialStateFile: "testdata/diff/003-unmask/initial3x.yaml",
+			stateFile:        "testdata/diff/003-unmask/kong3x.yaml",
+			envVars:          diffEnvVars,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			for k, v := range tc.envVars {
+				os.Setenv(k, v)
+				defer func(k string) {
+					os.Unsetenv(k)
+				}(k)
+			}
+			runWhen(t, "kong", ">=3.0.0")
+			teardown := setup(t)
+			defer teardown(t)
+
+			// initialize state
+			assert.NoError(t, sync(tc.initialStateFile))
+
+			out, err := diff(tc.stateFile, "--no-mask-deck-env-vars-value")
+			assert.NoError(t, err)
+			assert.Equal(t, expectedOutputUnMasked, out)
 		})
 	}
 }

tests/integration/testdata/diff/001-not-existing-workspace/kong3x.yaml
@@ -2,14 +2,7 @@ _format_version: "3.0"
 _workspace: test
 services:
   - name: svc1
-    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
-    host: ${{ env "DECK_SVC1_HOSTNAME" }}
-    tags:
-      - ${{ env "DECK_SVC1_HOSTNAME" }} is an external host. I like mockbin.org!
-      - foo:foo
-      - baz:${{ env "DECK_BAZZ" }}
-      - another:${{ env "DECK_BAZZ" }}
-      - bar:${{ env "DECK_BARR" }}
+    host: mockbin.org
 plugins:
   - name: rate-limiting
     id: a1368a28-cb5c-4eee-86d8-03a6bdf94b5e

tests/integration/testdata/diff/002-mask/initial.yaml
@@ -1,0 +1,5 @@
+_format_version: "1.1"
+services:
+  - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
+    host: ${{ env "DECK_SVC1_HOSTNAME" }}

tests/integration/testdata/diff/002-mask/initial3x.yaml
@@ -1,0 +1,5 @@
+_format_version: "3.0"
+services:
+  - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
+    host: ${{ env "DECK_SVC1_HOSTNAME" }}

tests/integration/testdata/diff/002-mask/kong.yaml
@@ -1,0 +1,16 @@
+_format_version: "1.1"
+services:
+  - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
+    host: ${{ env "DECK_SVC1_HOSTNAME" }}
+    tags:
+      - ${{ env "DECK_SVC1_HOSTNAME" }} is an external host. I like mockbin.org!
+      - foo:foo
+      - baz:${{ env "DECK_BAZZ" }}
+      - another:${{ env "DECK_BAZZ" }}
+      - bar:${{ env "DECK_BARR" }}
+plugins:
+  - name: rate-limiting
+    id: a1368a28-cb5c-4eee-86d8-03a6bdf94b5e
+    config:
+      minute: 123

tests/integration/testdata/diff/002-mask/kong3x.yaml
@@ -1,0 +1,16 @@
+_format_version: "3.0"
+services:
+  - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
+    host: ${{ env "DECK_SVC1_HOSTNAME" }}
+    tags:
+      - ${{ env "DECK_SVC1_HOSTNAME" }} is an external host. I like mockbin.org!
+      - foo:foo
+      - baz:${{ env "DECK_BAZZ" }}
+      - another:${{ env "DECK_BAZZ" }}
+      - bar:${{ env "DECK_BARR" }}
+plugins:
+  - name: rate-limiting
+    id: a1368a28-cb5c-4eee-86d8-03a6bdf94b5e
+    config:
+      minute: 123

tests/integration/testdata/diff/003-unmask/initial.yaml
@@ -1,0 +1,5 @@
+_format_version: "1.1"
+services:
+  - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
+    host: mockbin.org

tests/integration/testdata/diff/003-unmask/initial3x.yaml
@@ -1,0 +1,5 @@
+_format_version: "3.0"
+services:
+  - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
+    host: mockbin.org

tests/integration/testdata/diff/003-unmask/kong.yaml
@@ -1,7 +1,10 @@
-_workspace: test
+_format_version: "1.1"
 services:
   - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
     host: mockbin.org
+    tags:
+      - test
 plugins:
   - name: rate-limiting
     id: a1368a28-cb5c-4eee-86d8-03a6bdf94b5e

tests/integration/testdata/diff/003-unmask/kong3x.yaml
@@ -1,7 +1,10 @@
-_workspace: test
+_format_version: "3.0"
 services:
   - name: svc1
+    id: 9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d
     host: mockbin.org
+    tags:
+      - test
 plugins:
   - name: rate-limiting
     id: a1368a28-cb5c-4eee-86d8-03a6bdf94b5e