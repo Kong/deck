e57ba6e665e8cb06bf8b2bcc1e368c8eef937dfd
Alan Boudreault <alan@alanb.ca>
2023-06-07 14:51:37 -0400

    chore(deps): bump go-kong to 0.43 (#936)

--- tests/integration/dump_test.go
+++ tests/integration/dump_test.go
@@ -8,7 +8,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Dump_SelectTags(t *testing.T) {
+func Test_Dump_SelectTags_30(t *testing.T) {
+	tests := []struct {
+		name         string
+		stateFile    string
+		expectedFile string
+	}{
+		{
+			name:         "dump with select-tags",
+			stateFile:    "testdata/dump/001-entities-with-tags/kong.yaml",
+			expectedFile: "testdata/dump/001-entities-with-tags/expected30.yaml",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			runWhen(t, "kong", ">=3.0.0 <3.1.0")
+			teardown := setup(t)
+			defer teardown(t)
+
+			assert.NoError(t, sync(tc.stateFile))
+
+			output, err := dump(
+				"--select-tag", "managed-by-deck",
+				"--select-tag", "org-unit-42",
+				"-o", "-",
+			)
+			assert.NoError(t, err)
+
+			expected, err := readFile(tc.expectedFile)
+			assert.NoError(t, err)
+			assert.Equal(t, output, expected)
+		})
+	}
+}
+
+func Test_Dump_SelectTags_3x(t *testing.T) {
 	tests := []struct {
 		name         string
 		stateFile    string
@@ -22,7 +56,7 @@ func Test_Dump_SelectTags(t *testing.T) {
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			runWhen(t, "kong", ">=3.0.0")
+			runWhen(t, "kong", ">=3.1.0")
 			teardown := setup(t)
 			defer teardown(t)
 
