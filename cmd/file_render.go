9cf04c7ac10bc3bf9021e220c41d9b0e060ba5bf
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-07-17 15:04:59 +0200

    fixes; review + args for files

--- cmd/file_render.go
+++ cmd/file_render.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/kong/deck/convert"
 	"github.com/spf13/cobra"
 )
@@ -10,17 +8,11 @@ import (
 var (
 	fileRenderCmdKongStateFile  []string
 	fileRenderCmdKongFileOutput string
-	fileRenderCmdStateFormat    string
 )
 
 func executeFileRenderCmd(_ *cobra.Command, _ []string) error {
-	destinationFormat, err := convert.ParseFormat(fileRenderCmdStateFormat)
-	if err != nil {
-		return err
-	}
-
 	return convert.Convert(fileRenderCmdKongStateFile, fileRenderCmdKongFileOutput,
-		convert.FormatDistributed, destinationFormat, true)
+		convert.FormatDistributed, convert.FormatKongGateway3x, true)
 }
 
 func newFileRenderCmd() *cobra.Command {
@@ -31,23 +23,21 @@ func newFileRenderCmd() *cobra.Command {
 		Args:  validateNoArgs,
 		RunE:  executeFileRenderCmd,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			if len(fileRenderCmdStateFormat) == 0 {
-				return fmt.Errorf("a state file with Kong's configuration " +
-					"must be specified using `-s`/`--state` flag")
+			fileRenderCmdKongStateFile = args
+			if len(fileRenderCmdKongStateFile) == 0 {
+				fileRenderCmdKongStateFile = []string{"-"} // default to stdin
 			}
 			return preRunSilenceEventsFlag()
 		},
 	}
 
-	renderCmd.Flags().StringSliceVarP(&fileRenderCmdKongStateFile,
-		"state", "s", []string{"-"}, "file(s) containing Kong's configuration.\n"+
-			"This flag can be specified multiple times for multiple files.\n"+
-			"Use `-` to read from stdin.")
 	renderCmd.Flags().StringVarP(&fileRenderCmdKongFileOutput, "output-file", "o",
 		"-", "file to which to write Kong's configuration."+
 			"Use `-` to write to stdout.")
-	renderCmd.Flags().StringVar(&fileRenderCmdStateFormat, "format",
-		"yaml", "output file format: json or yaml.")
+
+	// TODO: support json output
+	// renderCmd.Flags().StringVar(&fileRenderCmdStateFormat, "format",
+	// 	"yaml", "output file format: json or yaml.")
 
 	return renderCmd
 }
