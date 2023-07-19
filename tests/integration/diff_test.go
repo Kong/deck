276e07c8bcaf00edaa650bdf7721c554aac5c536
DanielRailean <daniel.railean1@gmail.com>
2023-07-19 11:30:42 +0200

    v3.0.0 tests.

--- tests/integration/diff_test.go
+++ tests/integration/diff_test.go
@@ -262,6 +262,198 @@ Summary:
 }
 
 `
+
+	expectedOutputUnMaskedJSON30x = `{
+	"changes": {
+		"creating": [
+			{
+				"name": "rate-limiting (global)",
+				"kind": "plugin",
+				"body": {
+					"new": {
+						"id": "a1368a28-cb5c-4eee-86d8-03a6bdf94b5e",
+						"name": "rate-limiting",
+						"config": {
+							"day": null,
+							"error_code": 429,
+							"header_name": null,
+							"hide_client_headers": false,
+							"hour": null,
+							"limit_by": "consumer",
+							"minute": 123,
+							"month": null,
+							"path": null,
+							"policy": "local",
+							"redis_database": 0,
+							"redis_host": null,
+							"redis_password": null,
+							"redis_port": 6379,
+							"redis_server_name": null,
+							"redis_ssl": false,
+							"redis_ssl_verify": false,
+							"redis_timeout": 2000,
+							"redis_username": null,
+							"second": null,
+							"year": null
+						},
+						"enabled": true,
+						"protocols": [
+							"grpc",
+							"grpcs",
+							"http",
+							"https"
+						]
+					},
+					"old": null
+				}
+			}
+		],
+		"updating": [
+			{
+				"name": "svc1",
+				"kind": "service",
+				"body": {
+					"new": {
+						"connect_timeout": 60000,
+						"enabled": true,
+						"host": "mockbin.org",
+						"id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d",
+						"name": "svc1",
+						"port": 80,
+						"protocol": "http",
+						"read_timeout": 60000,
+						"retries": 5,
+						"write_timeout": 60000,
+						"tags": [
+							"test"
+						]
+					},
+					"old": {
+						"connect_timeout": 60000,
+						"enabled": true,
+						"host": "mockbin.org",
+						"id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d",
+						"name": "svc1",
+						"port": 80,
+						"protocol": "http",
+						"read_timeout": 60000,
+						"retries": 5,
+						"write_timeout": 60000
+					}
+				}
+			}
+		],
+		"deleting": []
+	},
+	"summary": {
+		"creating": 1,
+		"updating": 1,
+		"deleting": 0,
+		"total": 2
+	},
+	"warnings": [],
+	"errors": []
+}
+
+`
+
+	expectedOutputMaskedJSON30x = `{
+	"changes": {
+		"creating": [
+			{
+				"name": "rate-limiting (global)",
+				"kind": "plugin",
+				"body": {
+					"new": {
+						"id": "a1368a28-cb5c-4eee-86d8-03a6bdf94b5e",
+						"name": "rate-limiting",
+						"config": {
+							"day": null,
+							"error_code": 429,
+							"header_name": null,
+							"hide_client_headers": false,
+							"hour": null,
+							"limit_by": "consumer",
+							"minute": 123,
+							"month": null,
+							"path": null,
+							"policy": "local",
+							"redis_database": 0,
+							"redis_host": null,
+							"redis_password": null,
+							"redis_port": 6379,
+							"redis_server_name": null,
+							"redis_ssl": false,
+							"redis_ssl_verify": false,
+							"redis_timeout": 2000,
+							"redis_username": null,
+							"second": null,
+							"year": null
+						},
+						"enabled": true,
+						"protocols": [
+							"grpc",
+							"grpcs",
+							"http",
+							"https"
+						]
+					},
+					"old": null
+				}
+			}
+		],
+		"updating": [
+			{
+				"name": "svc1",
+				"kind": "service",
+				"body": {
+					"new": {
+						"connect_timeout": 60000,
+						"enabled": true,
+						"host": "[masked]",
+						"id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d",
+						"name": "svc1",
+						"port": 80,
+						"protocol": "http",
+						"read_timeout": 60000,
+						"retries": 5,
+						"write_timeout": 60000,
+						"tags": [
+							"[masked] is an external host. I like [masked]!",
+							"foo:foo",
+							"baz:[masked]",
+							"another:[masked]",
+							"bar:[masked]"
+						]
+					},
+					"old": {
+						"connect_timeout": 60000,
+						"enabled": true,
+						"host": "[masked]",
+						"id": "9ecf5708-f2f4-444e-a4c7-fcd3a57f9a6d",
+						"name": "svc1",
+						"port": 80,
+						"protocol": "http",
+						"read_timeout": 60000,
+						"retries": 5,
+						"write_timeout": 60000
+					}
+				}
+			}
+		],
+		"deleting": []
+	},
+	"summary": {
+		"creating": 1,
+		"updating": 1,
+		"deleting": 0,
+		"total": 2
+	},
+	"warnings": [],
+	"errors": []
+}
+
+`
 )
 
 // test scope:
@@ -408,6 +600,23 @@ func Test_Diff_Masked_NewerThan3x(t *testing.T) {
 			for k, v := range tc.envVars {
 				t.Setenv(k, v)
 			}
+			runWhen(t, "kong", ">=3.0.0 <3.1.0")
+			teardown := setup(t)
+			defer teardown(t)
+
+			// initialize state
+			assert.NoError(t, sync(tc.initialStateFile))
+
+			out, err := diff(tc.stateFile, "--json-output")
+			assert.NoError(t, err)
+			assert.Equal(t, expectedOutputMaskedJSON30x, out)
+		})
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			for k, v := range tc.envVars {
+				t.Setenv(k, v)
+			}
 			runWhen(t, "kong", ">=3.1.0")
 			teardown := setup(t)
 			defer teardown(t)
@@ -509,7 +718,23 @@ func Test_Diff_Unmasked_NewerThan31(t *testing.T) {
 			assert.Equal(t, expectedOutputUnMasked, out)
 		})
 	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			for k, v := range tc.envVars {
+				t.Setenv(k, v)
+			}
+			runWhen(t, "kong", ">=3.0.0 <3.1.0")
+			teardown := setup(t)
+			defer teardown(t)
 
+			// initialize state
+			assert.NoError(t, sync(tc.initialStateFile))
+
+			out, err := diff(tc.stateFile, "--no-mask-deck-env-vars-value", "--json-output")
+			assert.NoError(t, err)
+			assert.Equal(t, expectedOutputUnMaskedJSON30x, out)
+		})
+	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			for k, v := range tc.envVars {
