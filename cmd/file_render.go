677198b6e70fb3d2f3eac17433cf28aafaa3b946
lena-larionova <54370747+lena-larionova@users.noreply.github.com>
2023-09-22 06:41:18 -0700

    docs(cli): Add a long description for file render (#1007)
    ---------
    
    Co-authored-by: Rick Spurgeon <rspurgeon@users.noreply.github.com>
    Co-authored-by: Thijs Schreijer <thijs@thijsschreijer.nl>

--- cmd/file_render.go
+++ cmd/file_render.go
@@ -27,10 +27,25 @@ func executeFileRenderCmd(_ *cobra.Command, _ []string) error {
 func newFileRenderCmd() *cobra.Command {
 	renderCmd := &cobra.Command{
 		Use:   "render",
-		Short: "Render the configuration as Kong declarative config",
-		Long:  ``,
-		Args:  cobra.ArbitraryArgs,
-		RunE:  executeFileRenderCmd,
+		Short: "Combines multiple complete configuration files and renders them as one Kong declarative config file.",
+		Long: `Combines multiple complete configuration files and renders them as one Kong
+declarative config file.
+
+This command renders a full declarative configuration in JSON or YAML format by assembling 
+multiple files and populating defaults and environment substitutions. 
+This command is useful to observe what configuration would be sent prior to synchronizing to 
+the gateway.
+ 
+In comparison to the "deck file merge" command, the render command accepts 
+complete configuration files, while "deck file merge" can operate on partial files.
+
+For example, the following command takes two input files and renders them as one 
+combined JSON file:
+
+  deck file render kong1.yml kong2.yml -o kong3 --format json
+`,
+		Args: cobra.ArbitraryArgs,
+		RunE: executeFileRenderCmd,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			fileRenderCmdKongStateFile = args
 			if len(fileRenderCmdKongStateFile) == 0 {
