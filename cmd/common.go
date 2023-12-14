bc61cd917cb0baf9c6f0b788e6d9fb6757c3fbdf
Travis Raines <571832+rainest@users.noreply.github.com>
2023-12-13 16:03:16 -0800

    feat: handle action prints in cmd

--- cmd/common.go
+++ cmd/common.go
@@ -306,6 +306,7 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 	if diffCmdNonZeroExitCode && totalOps > 0 {
 		os.Exit(exitCodeDiffDetection)
 	}
+	// NOTE TRC apparently we handle json output printing here, in syncmain?
 	if enableJSONOutput {
 		jsonOutputBytes, jsonErr := json.MarshalIndent(jsonOutput, "", "\t")
 		if jsonErr != nil {
@@ -374,8 +375,20 @@ func performDiff(ctx context.Context, currentState, targetState *state.KongState
 	}
 
 	stats, errs, changes := s.Solve(ctx, parallelism, dry, enableJSONOutput)
+	// TODO TRC not familiar with the differences between this and jsonoutput. jsonoutput is unfortunately a module
+	// global and idk how we handle actually printing it. apparently _it_ prints in syncmain and this prints here,
+	// because organic inconsistency. we _only_ use changes currently for json out
 	// print stats before error to report completed operations
 	if !enableJSONOutput {
+		for _, c := range changes.Creating {
+			cprint.CreatePrintln("creating", c.Kind, c.Name)
+		}
+		for _, c := range changes.Updating {
+			cprint.UpdatePrintln("updating", c.Kind, c.Name, c.Diff)
+		}
+		for _, c := range changes.Deleting {
+			cprint.DeletePrintln("deleting", c.Kind, c.Name)
+		}
 		printStats(stats)
 	}
 	if errs != nil {
