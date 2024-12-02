d06788a2a8c295052b42167d65db4bf92432a9c8
Michael Heap <m@michaelheap.com>
2024-12-02 17:39:14 +0000

    feat(apply): add deck gateway apply command
    The `deck gateway apply` command allows you to apply partial
    configuration to a running Gateway instance.
    
    To do this, it runs a `sync` with the NoDeletes flag enabled.
    This means that only new and existing resources are updated.
    Existing resources that do not exist in the declarative
    configuration file are left untouched.

--- cmd/common.go
+++ cmd/common.go
@@ -131,7 +131,7 @@ func RemoveConsumerPlugins(targetContentPlugins []file.FPlugin) []file.FPlugin {
 }
 
 func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
-	delay int, workspace string, enableJSONOutput bool,
+	delay int, workspace string, enableJSONOutput bool, noDeletes bool,
 ) error {
 	// read target file
 	if enableJSONOutput {
@@ -157,6 +157,13 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 	}
 
 	cmd := "sync"
+
+	isPartialApply := false
+	if noDeletes {
+		cmd = "apply"
+		isPartialApply = true
+	}
+
 	if dry {
 		cmd = "diff"
 	}
@@ -215,6 +222,14 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return fmt.Errorf("parsing Kong version: %w", err)
 	}
 
+	// Are we running enterprise?
+	v, err := kong.ParseSemanticVersion(kongVersion)
+	if err != nil {
+		return fmt.Errorf("parsing Kong version: %w", err)
+	}
+
+	isKongEnterprise := v.IsKongGatewayEnterprise()
+
 	if parsedKongVersion.GTE(reconcilerUtils.Kong300Version) &&
 		targetContent.FormatVersion != utils.FormatVersion30 {
 		formatVersion := targetContent.FormatVersion
@@ -248,21 +263,24 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return err
 	}
 
-	dumpConfig.LookUpSelectorTagsConsumerGroups, err = determineLookUpSelectorTagsConsumerGroups(*targetContent)
-	if err != nil {
-		return fmt.Errorf("error determining lookup selector tags for consumer groups: %w", err)
-	}
-
-	if dumpConfig.LookUpSelectorTagsConsumerGroups != nil {
-		consumerGroupsGlobal, err := dump.GetAllConsumerGroups(ctx, kongClient, dumpConfig.LookUpSelectorTagsConsumerGroups)
+	// Consumer groups are an enterprise 3.4+ feature
+	if parsedKongVersion.GTE(reconcilerUtils.Kong340Version) && isKongEnterprise {
+		dumpConfig.LookUpSelectorTagsConsumerGroups, err = determineLookUpSelectorTagsConsumerGroups(*targetContent)
 		if err != nil {
-			return fmt.Errorf("error retrieving global consumer groups via lookup selector tags: %w", err)
+			return fmt.Errorf("error determining lookup selector tags for consumer groups: %w", err)
 		}
-		for _, c := range consumerGroupsGlobal {
-			targetContent.ConsumerGroups = append(targetContent.ConsumerGroups,
-				file.FConsumerGroupObject{ConsumerGroup: *c.ConsumerGroup})
+
+		if dumpConfig.LookUpSelectorTagsConsumerGroups != nil || isPartialApply {
+			consumerGroupsGlobal, err := dump.GetAllConsumerGroups(ctx, kongClient, dumpConfig.LookUpSelectorTagsConsumerGroups)
 			if err != nil {
-				return fmt.Errorf("error adding global consumer group %v: %w", *c.ConsumerGroup.Name, err)
+				return fmt.Errorf("error retrieving global consumer groups via lookup selector tags: %w", err)
+			}
+			for _, c := range consumerGroupsGlobal {
+				targetContent.ConsumerGroups = append(targetContent.ConsumerGroups,
+					file.FConsumerGroupObject{ConsumerGroup: *c.ConsumerGroup})
+				if err != nil {
+					return fmt.Errorf("error adding global consumer group %v: %w", *c.ConsumerGroup.Name, err)
+				}
 			}
 		}
 	}
@@ -272,7 +290,7 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return fmt.Errorf("error determining lookup selector tags for consumers: %w", err)
 	}
 
-	if dumpConfig.LookUpSelectorTagsConsumers != nil {
+	if dumpConfig.LookUpSelectorTagsConsumers != nil || isPartialApply {
 		consumersGlobal, err := dump.GetAllConsumers(ctx, kongClient, dumpConfig.LookUpSelectorTagsConsumers)
 		if err != nil {
 			return fmt.Errorf("error retrieving global consumers via lookup selector tags: %w", err)
@@ -290,7 +308,7 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return fmt.Errorf("error determining lookup selector tags for routes: %w", err)
 	}
 
-	if dumpConfig.LookUpSelectorTagsRoutes != nil {
+	if dumpConfig.LookUpSelectorTagsRoutes != nil || isPartialApply {
 		routesGlobal, err := dump.GetAllRoutes(ctx, kongClient, dumpConfig.LookUpSelectorTagsRoutes)
 		if err != nil {
 			return fmt.Errorf("error retrieving global routes via lookup selector tags: %w", err)
@@ -308,7 +326,7 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return fmt.Errorf("error determining lookup selector tags for services: %w", err)
 	}
 
-	if dumpConfig.LookUpSelectorTagsServices != nil {
+	if dumpConfig.LookUpSelectorTagsServices != nil || isPartialApply {
 		servicesGlobal, err := dump.GetAllServices(ctx, kongClient, dumpConfig.LookUpSelectorTagsServices)
 		if err != nil {
 			return fmt.Errorf("error retrieving global services via lookup selector tags: %w", err)
@@ -373,7 +391,7 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 	}
 
 	totalOps, err := performDiff(
-		ctx, currentState, targetState, dry, parallelism, delay, kongClient, mode == modeKonnect, enableJSONOutput)
+		ctx, currentState, targetState, dry, parallelism, delay, kongClient, mode == modeKonnect, enableJSONOutput, noDeletes)
 	if err != nil {
 		if enableJSONOutput {
 			var errs reconcilerUtils.ErrArray
@@ -502,7 +520,7 @@ func fetchCurrentState(ctx context.Context, client *kong.Client, dumpConfig dump
 
 func performDiff(ctx context.Context, currentState, targetState *state.KongState,
 	dry bool, parallelism int, delay int, client *kong.Client, isKonnect bool,
-	enableJSONOutput bool,
+	enableJSONOutput bool, noDeletes bool,
 ) (int, error) {
 	s, err := diff.NewSyncer(diff.SyncerOpts{
 		CurrentState:  currentState,
@@ -511,6 +529,7 @@ func performDiff(ctx context.Context, currentState, targetState *state.KongState
 		StageDelaySec: delay,
 		NoMaskValues:  noMaskValues,
 		IsKonnect:     isKonnect,
+		NoDeletes:     noDeletes,
 	})
 	if err != nil {
 		return 0, err
