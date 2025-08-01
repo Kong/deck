07775c8f61f6906addba02600fc29d0585bb9480
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2025-08-01 12:48:30 +0530

    style: spelling consistency

--- sanitize/utils.go
+++ sanitize/utils.go
@@ -14,18 +14,3 @@ func shouldSkipSanitization(fieldName string, exemptionMap map[string]struct{}) 
 
 	return false
 }
-
-func (s *Sanitizer) shouldSkipSanitization(fieldName string, exemptionMap map[string]struct{}) bool {
-	if exemptionMap != nil {
-		if _, exempt := exemptionMap[fieldName]; exempt {
-			return true
-		}
-	}
-
-	// checking for config-level exemptions
-	if _, exempt := configLevelExemptedFields[fieldName]; exempt {
-		return true
-	}
-
-	return false
-}
