6a550077a80c34915c76f4a6e58f4f869927a513
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2025-08-19 17:18:29 +0530

    tests: corrected test-utils

--- tests/integration/test_utils.go
+++ tests/integration/test_utils.go
@@ -108,6 +108,7 @@ func runWhen(t *testing.T, mode string, semverRange string) {
 	case "kong":
 		skipWhenKonnect(t)
 		kong.RunWhenKong(t, semverRange)
+		kong.RunWhenKongRouterFlavor(t, "traditional_compatible")
 	case "kong-expressions":
 		skipWhenKonnect(t)
 		kong.RunWhenKong(t, semverRange)
@@ -115,6 +116,7 @@ func runWhen(t *testing.T, mode string, semverRange string) {
 	case "enterprise":
 		skipWhenKonnect(t)
 		kong.RunWhenEnterprise(t, semverRange, kong.RequiredFeatures{})
+		kong.RunWhenKongRouterFlavor(t, "traditional_compatible")
 	case "enterprise-expressions":
 		skipWhenKonnect(t)
 		kong.RunWhenEnterprise(t, semverRange, kong.RequiredFeatures{})
