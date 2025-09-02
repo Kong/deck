aa49dd226f9c6059d8b8775859316996f4bedf08
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2025-09-02 16:38:56 +0530

    temp: removed sync vault test for now

--- tests/integration/sync_test.go
+++ tests/integration/sync_test.go
@@ -4,8 +4,6 @@ package integration
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -4251,175 +4249,175 @@ u8S6yNlu2Q==
 // an HTTPS client is created using the `caCert` used to sign the
 // deployed certificate, and then a GET is performed to test the
 // proxy functionality, which should return a 200.
-func Test_Sync_Vault(t *testing.T) {
-	// setup stage
-	client, err := getTestClient()
-	require.NoError(t, err)
+// func Test_Sync_Vault(t *testing.T) {
+// 	// setup stage
+// 	client, err := getTestClient()
+// 	require.NoError(t, err)
 
-	tests := []struct {
-		name            string
-		kongFile        string
-		initialKongFile string
-		expectedState   utils.KongRawState
-		runWhen         string
-	}{
-		{
-			name:     "create an SSL service/route using an ENV vault <3.11.0",
-			kongFile: "testdata/sync/012-vaults/kong3x.yaml",
-			expectedState: utils.KongRawState{
-				Vaults: []*kong.Vault{
-					{
-						Name:        kong.String("env"),
-						Prefix:      kong.String("my-env-vault"),
-						Description: kong.String("ENV vault for secrets"),
-						Config: kong.Configuration{
-							"prefix": "MY_SECRET_",
-						},
-					},
-				},
-				Services: []*kong.Service{
-					{
-						ID:             kong.String("58076db2-28b6-423b-ba39-a797193017f7"),
-						Name:           kong.String("svc1"),
-						ConnectTimeout: kong.Int(60000),
-						Host:           kong.String("httpbin.org"),
-						Port:           kong.Int(80),
-						Path:           kong.String("/status/200"),
-						Protocol:       kong.String("http"),
-						ReadTimeout:    kong.Int(60000),
-						Retries:        kong.Int(5),
-						WriteTimeout:   kong.Int(60000),
-						Tags:           nil,
-						Enabled:        kong.Bool(true),
-					},
-				},
-				Routes: route1_20x,
-				Certificates: []*kong.Certificate{
-					{
-						ID:   kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
-						Cert: kong.String("{vault://my-env-vault/cert}"),
-						Key:  kong.String("{vault://my-env-vault/key}"),
-					},
-				},
-				SNIs: []*kong.SNI{
-					{
-						Name: kong.String("localhost"),
-						Certificate: &kong.Certificate{
-							ID: kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
-						},
-					},
-				},
-			},
-			runWhen: ">=3.0.0 <3.11.0",
-		},
-		{
-			name:     "create an SSL service/route using an ENV vault >=3.11.0",
-			kongFile: "testdata/sync/012-vaults/kong3x.yaml",
-			expectedState: utils.KongRawState{
-				Vaults: []*kong.Vault{
-					{
-						Name:        kong.String("env"),
-						Prefix:      kong.String("my-env-vault"),
-						Description: kong.String("ENV vault for secrets"),
-						Config: kong.Configuration{
-							"base64_decode": false,
-							"prefix":        "MY_SECRET_",
-						},
-					},
-				},
-				Services: []*kong.Service{
-					{
-						ID:             kong.String("58076db2-28b6-423b-ba39-a797193017f7"),
-						Name:           kong.String("svc1"),
-						ConnectTimeout: kong.Int(60000),
-						Host:           kong.String("httpbin.org"),
-						Port:           kong.Int(80),
-						Path:           kong.String("/status/200"),
-						Protocol:       kong.String("http"),
-						ReadTimeout:    kong.Int(60000),
-						Retries:        kong.Int(5),
-						WriteTimeout:   kong.Int(60000),
-						Tags:           nil,
-						Enabled:        kong.Bool(true),
-					},
-				},
-				Routes: route1_20x,
-				Certificates: []*kong.Certificate{
-					{
-						ID:   kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
-						Cert: kong.String("{vault://my-env-vault/cert}"),
-						Key:  kong.String("{vault://my-env-vault/key}"),
-					},
-				},
-				SNIs: []*kong.SNI{
-					{
-						Name: kong.String("localhost"),
-						Certificate: &kong.Certificate{
-							ID: kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
-						},
-					},
-				},
-			},
-			runWhen: ">=3.11.0",
-		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			runWhen(t, "enterprise", tc.runWhen)
-			setup(t)
+// 	tests := []struct {
+// 		name            string
+// 		kongFile        string
+// 		initialKongFile string
+// 		expectedState   utils.KongRawState
+// 		runWhen         string
+// 	}{
+// 		{
+// 			name:     "create an SSL service/route using an ENV vault <3.11.0",
+// 			kongFile: "testdata/sync/012-vaults/kong3x.yaml",
+// 			expectedState: utils.KongRawState{
+// 				Vaults: []*kong.Vault{
+// 					{
+// 						Name:        kong.String("env"),
+// 						Prefix:      kong.String("my-env-vault"),
+// 						Description: kong.String("ENV vault for secrets"),
+// 						Config: kong.Configuration{
+// 							"prefix": "MY_SECRET_",
+// 						},
+// 					},
+// 				},
+// 				Services: []*kong.Service{
+// 					{
+// 						ID:             kong.String("58076db2-28b6-423b-ba39-a797193017f7"),
+// 						Name:           kong.String("svc1"),
+// 						ConnectTimeout: kong.Int(60000),
+// 						Host:           kong.String("httpbin.org"),
+// 						Port:           kong.Int(80),
+// 						Path:           kong.String("/status/200"),
+// 						Protocol:       kong.String("http"),
+// 						ReadTimeout:    kong.Int(60000),
+// 						Retries:        kong.Int(5),
+// 						WriteTimeout:   kong.Int(60000),
+// 						Tags:           nil,
+// 						Enabled:        kong.Bool(true),
+// 					},
+// 				},
+// 				Routes: route1_20x,
+// 				Certificates: []*kong.Certificate{
+// 					{
+// 						ID:   kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
+// 						Cert: kong.String("{vault://my-env-vault/cert}"),
+// 						Key:  kong.String("{vault://my-env-vault/key}"),
+// 					},
+// 				},
+// 				SNIs: []*kong.SNI{
+// 					{
+// 						Name: kong.String("localhost"),
+// 						Certificate: &kong.Certificate{
+// 							ID: kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
+// 						},
+// 					},
+// 				},
+// 			},
+// 			runWhen: ">=3.0.0 <3.11.0",
+// 		},
+// 		{
+// 			name:     "create an SSL service/route using an ENV vault >=3.11.0",
+// 			kongFile: "testdata/sync/012-vaults/kong3x.yaml",
+// 			expectedState: utils.KongRawState{
+// 				Vaults: []*kong.Vault{
+// 					{
+// 						Name:        kong.String("env"),
+// 						Prefix:      kong.String("my-env-vault"),
+// 						Description: kong.String("ENV vault for secrets"),
+// 						Config: kong.Configuration{
+// 							"base64_decode": false,
+// 							"prefix":        "MY_SECRET_",
+// 						},
+// 					},
+// 				},
+// 				Services: []*kong.Service{
+// 					{
+// 						ID:             kong.String("58076db2-28b6-423b-ba39-a797193017f7"),
+// 						Name:           kong.String("svc1"),
+// 						ConnectTimeout: kong.Int(60000),
+// 						Host:           kong.String("httpbin.org"),
+// 						Port:           kong.Int(80),
+// 						Path:           kong.String("/status/200"),
+// 						Protocol:       kong.String("http"),
+// 						ReadTimeout:    kong.Int(60000),
+// 						Retries:        kong.Int(5),
+// 						WriteTimeout:   kong.Int(60000),
+// 						Tags:           nil,
+// 						Enabled:        kong.Bool(true),
+// 					},
+// 				},
+// 				Routes: route1_20x,
+// 				Certificates: []*kong.Certificate{
+// 					{
+// 						ID:   kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
+// 						Cert: kong.String("{vault://my-env-vault/cert}"),
+// 						Key:  kong.String("{vault://my-env-vault/key}"),
+// 					},
+// 				},
+// 				SNIs: []*kong.SNI{
+// 					{
+// 						Name: kong.String("localhost"),
+// 						Certificate: &kong.Certificate{
+// 							ID: kong.String("13c562a1-191c-4464-9b18-e5222b46035b"),
+// 						},
+// 					},
+// 				},
+// 			},
+// 			runWhen: ">=3.11.0",
+// 		},
+// 	}
+// 	for _, tc := range tests {
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			runWhen(t, "enterprise", tc.runWhen)
+// 			setup(t)
 
-			require.NoError(t, sync(context.Background(), tc.kongFile))
-			testKongState(t, client, false, false, tc.expectedState, nil)
+// 			require.NoError(t, sync(context.Background(), tc.kongFile))
+// 			testKongState(t, client, false, false, tc.expectedState, nil)
 
-			// Kong proxy may need a bit to be ready.
-			time.Sleep(time.Second * 5)
+// 			// Kong proxy may need a bit to be ready.
+// 			time.Sleep(time.Second * 5)
 
-			// build simple http client
-			client := &http.Client{}
+// 			// build simple http client
+// 			client := &http.Client{}
 
-			// use simple http client with https should result
-			// in a failure due missing certificate.
-			_, err := client.Get("https://localhost:8443/r1")
-			require.Error(t, err)
+// 			// use simple http client with https should result
+// 			// in a failure due missing certificate.
+// 			_, err := client.Get("https://localhost:8443/r1")
+// 			require.Error(t, err)
 
-			// use transport with wrong CA cert this should result
-			// in a failure due to unknown authority.
-			badCACertPool := x509.NewCertPool()
-			badCACertPool.AppendCertsFromPEM(badCACertPEM)
+// 			// use transport with wrong CA cert this should result
+// 			// in a failure due to unknown authority.
+// 			badCACertPool := x509.NewCertPool()
+// 			badCACertPool.AppendCertsFromPEM(badCACertPEM)
 
-			client = &http.Client{
-				Transport: &http.Transport{
-					TLSClientConfig: &tls.Config{
-						RootCAs:    badCACertPool,
-						ClientAuth: tls.RequireAndVerifyClientCert,
-					},
-				},
-			}
+// 			client = &http.Client{
+// 				Transport: &http.Transport{
+// 					TLSClientConfig: &tls.Config{
+// 						RootCAs:    badCACertPool,
+// 						ClientAuth: tls.RequireAndVerifyClientCert,
+// 					},
+// 				},
+// 			}
 
-			_, err = client.Get("https://localhost:8443/r1")
-			require.Error(t, err)
+// 			_, err = client.Get("https://localhost:8443/r1")
+// 			require.Error(t, err)
 
-			// use transport with good CA cert should pass
-			// if referenced secrets are resolved correctly
-			// using the ENV vault.
-			goodCACertPool := x509.NewCertPool()
-			goodCACertPool.AppendCertsFromPEM(goodCACertPEM)
+// 			// use transport with good CA cert should pass
+// 			// if referenced secrets are resolved correctly
+// 			// using the ENV vault.
+// 			goodCACertPool := x509.NewCertPool()
+// 			goodCACertPool.AppendCertsFromPEM(goodCACertPEM)
 
-			client = &http.Client{
-				Transport: &http.Transport{
-					TLSClientConfig: &tls.Config{
-						RootCAs:    goodCACertPool,
-						ClientAuth: tls.RequireAndVerifyClientCert,
-					},
-				},
-			}
+// 			client = &http.Client{
+// 				Transport: &http.Transport{
+// 					TLSClientConfig: &tls.Config{
+// 						RootCAs:    goodCACertPool,
+// 						ClientAuth: tls.RequireAndVerifyClientCert,
+// 					},
+// 				},
+// 			}
 
-			res, err := client.Get("https://localhost:8443/r1")
-			require.NoError(t, err)
-			assert.Equal(t, http.StatusOK, res.StatusCode)
-		})
-	}
-}
+// 			res, err := client.Get("https://localhost:8443/r1")
+// 			require.NoError(t, err)
+// 			assert.Equal(t, http.StatusOK, res.StatusCode)
+// 		})
+// 	}
+// }
 
 // test scope:
 //   - 2.8.x
