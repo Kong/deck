1a7238b88d97b29b54349e27a4f2ff27181faeab
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2025-10-17 10:25:58 +0530

    chore: removed stray comment

--- cmd/common.go
+++ cmd/common.go
@@ -413,7 +413,6 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 	}
 	targetState, err := state.Get(rawState)
 	if err != nil {
-		fmt.Println("Error in creating target state from Kong:", err)
 		return err
 	}
 
