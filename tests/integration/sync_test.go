7791600ea1163b54373e0ea874f62dd5a1890681
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2025-11-25 18:19:49 +0530

    chore: added integration tests for service tls_sans

--- tests/integration/sync_test.go
+++ tests/integration/sync_test.go
@@ -10454,3 +10454,57 @@ func Test_Sync_BasicAuth_SkipHash_Konnect(t *testing.T) {
 		})
 	}
 }
+
+func Test_Sync_Services_TLS_Sans(t *testing.T) {
+	runWhenEnterpriseOrKonnect(t, ">=3.10.0")
+	setup(t)
+	ctx := context.Background()
+
+	tests := []struct {
+		name        string
+		initialFile string
+		updateFile  string
+	}{
+		{
+			name:        "create a service with TLS SANs",
+			initialFile: "testdata/sync/048-service-tls-sans/kong.yaml",
+		},
+		{
+			name:        "update an existing service with TLS SANs - protocol https",
+			initialFile: "testdata/sync/048-service-tls-sans/no-tls-https.yaml",
+			updateFile:  "testdata/sync/048-service-tls-sans/kong.yaml",
+		},
+		{
+			name:        "update an existing service with TLS SANs - protocol http",
+			initialFile: "testdata/sync/048-service-tls-sans/no-tls-http.yaml",
+			updateFile:  "testdata/sync/048-service-tls-sans/kong.yaml",
+		},
+		{
+			name:        "remove TLS SANs from existing service",
+			initialFile: "testdata/sync/048-service-tls-sans/kong.yaml",
+			updateFile:  "testdata/sync/048-service-tls-sans/no-tls-https.yaml",
+		},
+		{
+			name:        "remove TLS SANs from existing service and change protocol to http",
+			initialFile: "testdata/sync/048-service-tls-sans/kong.yaml",
+			updateFile:  "testdata/sync/048-service-tls-sans/no-tls-http.yaml",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			reset(t)
+			require.NoError(t, sync(ctx, tc.initialFile))
+			// resync with no error
+			require.NoError(t, sync(ctx, tc.initialFile))
+
+			if tc.updateFile == "" {
+				return
+			}
+			// update
+			require.NoError(t, sync(ctx, tc.updateFile))
+			// resync with no error
+			require.NoError(t, sync(ctx, tc.updateFile))
+		})
+	}
+}
