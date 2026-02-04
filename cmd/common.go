9be79ce7e6849c9496d49ade73c9ebd3ee17624b
shivay <shivay.gupta@konghq.com>
2026-02-04 13:56:02 +0530

    fix: resolved linting issue

--- cmd/common.go
+++ cmd/common.go
@@ -156,9 +156,9 @@ func syncMain(ctx context.Context, filenames []string, dry bool, parallelism,
 		return err
 	}
 
-	if dumpConfig.SkipConsumers && targetContent.Info != nil &&
-		targetContent.Info.LookUpSelectorTags != nil &&
-		(targetContent.Info.LookUpSelectorTags.Consumers != nil || targetContent.Info.LookUpSelectorTags.ConsumerGroups != nil) {
+	if dumpConfig.SkipConsumers && targetContent.Info != nil && targetContent.Info.LookUpSelectorTags != nil &&
+		(targetContent.Info.LookUpSelectorTags.Consumers != nil ||
+			targetContent.Info.LookUpSelectorTags.ConsumerGroups != nil) {
 		return errors.New("cannot use --skip-consumers with default lookup tags for consumers or consumer groups")
 	}
 
