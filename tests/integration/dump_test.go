0a6f72b9aa9fcd02c4249f9e75b70c96e8c6343c
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2026-06-03 15:55:43 +0530

    tests: fixed konnect dump testdata

--- tests/integration/dump_test.go
+++ tests/integration/dump_test.go
@@ -1573,7 +1573,7 @@ func Test_Dump_CustomPluginDefinitions(t *testing.T) {
 	require.NoError(t, sync(ctx, "testdata/sync/055-custom-plugin-definitions/kong.yaml", "--include-plugin-definitions"))
 
 	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
+		t.Run(tc.name+" "+tc.runWhen+" "+tc.runWhenVersion, func(t *testing.T) {
 			runWhen(t, tc.runWhen, tc.runWhenVersion)
 
 			output, err := dump(tc.dumpFlags...)
